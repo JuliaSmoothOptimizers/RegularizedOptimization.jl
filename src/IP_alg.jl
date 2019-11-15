d88dc85b89cda041795d34c261204e999c1274cf
rjbaraldi <rbaraldi@uw.edu>
2019-11-15 12:56:20 -0800

    more testing for IP_alg

--- src/IP_alg.jl
+++ src/IP_alg.jl
@@ -140,11 +140,10 @@ function IntPt_TR(x, zl, zu,mu,params, options)
             FO_options.gk = ∇Phi
             FO_options.xk = x
             FO_options.σ_TR = trrad
-            objInner(u, ν)= ϕk(u, ν)
             funProj(s, Δ)= χ_projector(s, Δ)
         end
         # funProj(s) = projector(s, trrad, tr_options.β^(-1))
-
+        objInner(u, ν)= ϕk(u, ν)
         (s, fsave, funEvals)= s_alg(objInner, zeros(size(x)), funProj, FO_options)
 
 
