4119e73af200432afdafd817b935f1da10deb66c
saravkin <saravkin@uw.edu>
2020-03-01 05:13:57 -0800

    added matlab test for prox

---
+++ Matlab/hardprox.m
@@ -0,0 +1,18 @@
+function [s,f] = hardprox(z, x, t, lambda, tau)
+%HARDPROX computes the prox of the sum of shifted 1-norm and interval
+%constraint for a scalar variable 
+
+fval = @(s) (s-z)^2/(2*t) + lambda*abs(s+x); 
+projbox = @(w) min(max(w,-tau),tau);
+
+s1 = 0; f1 = fval(s1);
+s2 = projbox(z-t*lambda); f2 = fval(s2);
+s3 = projbox(z+t*lambda); f3 = fval(s3);
+smat = [s1, s2, s3];
+fvec = [f1; f2; f3];
+
+[f, idx] = min(fvec);
+s = smat(:, idx);
+
+end
+
---
+++ Matlab/hardproxtest.m
@@ -0,0 +1,17 @@
+x = 10*randn(1); 
+z = 5*randn(1);
+t = 20*rand(1);
+lambda = 10*rand(1); 
+tau = 3*rand(1);
+
+[s,f] = hardprox(z, x, t, lambda, tau);
+
+cvx_precision high
+cvx_begin
+    variable s_cvx(1)
+    minimize( (s_cvx-z)^2/(2*t) + lambda*abs(s_cvx+x))
+    subject to
+        -tau <= s_cvx <= tau
+cvx_end
+
+norm(s_cvx - s)
