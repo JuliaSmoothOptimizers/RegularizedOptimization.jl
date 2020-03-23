482b62a68ffada6dc67dcf48b5b0b074ad079d1a
rjbaraldi <rbaraldi@uw.edu>
2020-03-23 16:22:56 -0500

    fixed prox computational errors

--- src/ProxB2.jl
+++ src/ProxB2.jl
@@ -1,10 +1,12 @@
-function [s,f] = hardproxB2(z, x, t, lambda, tau)
+export hardproxB2
+
+function hardproxB2(z, x, t, lambda, tau)
 # %HARDPROXB2 computes the prox of the sum of shifted 1-norm and L2
 # %constraint for a scalar variable
 
-fval(s)=norm(s-z)^2/(2*t) + lambda*norm(s+x,1)
-projbox(y)= min(max(y, z-lambda*t),z+lambda*t) # different since through dual
-froot(eta)= eta - norm(projbox((-x)*(eta/tau)))
+fval(s)=norm(s.-z).^2./(2.*t) .+ lambda.*norm(s.+x,1)
+projbox(y)= min(max(y, z.-lambda.*t),z.+lambda.*t) # different since through dual
+froot(eta)= eta - norm(projbox((-x).*(eta/tau)))
 
 
 # %do the 2 norm projection
@@ -14,22 +16,22 @@ if (norm(y1)<= tau)
     str = 'y in tau'
 else
     eta = find_zero(froot, tau)
-    y = projbox((-x)*(eta/tau))
+    y = projbox((-x).*(eta/tau))
     str = 'y root'
 
 end
 
-if(norm(y) <=tau)
+if(norm(y)<=tau)
     s = y
     str2 = 'within tau'
 else
-    s = tau*y/norm(y)
+    s = tau.*y./norm(y)
     str2 = 'out tau'
 end
 f = fval(s)
 
 # fprintf('Y-meth: %s    s-meth: %s    s: %1.4f   y:%1.4f\n', str, str2, s, y);
-
+return s,f
 
 
 end
--- src/ProxBinf.jl
+++ src/ProxBinf.jl
@@ -1,17 +1,23 @@
-function [s,f] = hardproxBinf(z, x, t, lambda, tau)
+export hardproxBinf
+
+function hardproxBinf(z, x, t, lambda, tau)
 # HARDPROX computes the prox of the sum of shifted 1-norm and interval
 # constraint for a scalar variable
 
-fval(s) = (s-z).^2/(2*t) + lambda*abs.(s+x)
+fval(s) = (s.-z).^2/(2*t) .+ lambda.*abs.(s.+x)
 projbox(w) = min.(max.(w,-tau),tau)
 
-s1 = 0; f1 = fval(s1);
-s2 = projbox(z-t*lambda); f2 = fval(s2)
-s3 = projbox(z+t*lambda); f3 = fval(s3)
-smat = [s1, s2, s3]
+s1 = 0
+f1 = fval(s1)
+s2 = projbox(z.-t.*lambda)
+f2 = fval(s2)
+s3 = projbox(z.+t.*lambda)
+f3 = fval(s3)
+smat = [s1; s2; s3]
 fvec = [f1; f2; f3]
 
-[f, idx] = min.(fvec)
-s = smat[:, idx]
+(f, idx) = min.(fvec)
+s = smat(:, idx)
 
+return s, f
 end
