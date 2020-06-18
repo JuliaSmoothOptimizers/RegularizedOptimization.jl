ca1eb924400de1457d38a919c921b8079e470a5b
rjbaraldi <rbaraldi@uw.edu>
2020-06-18 13:47:51 -0700

    I think the precompile is messing up?

--- src/IP_alg.jl
+++ src/IP_alg.jl
@@ -200,7 +200,7 @@ function IntPt_TR(
 
             if ~isempty(methods(Bk))
                 ∇²ϕ(d) = Bk(d) + Diagonal(zkl ./ (xk - l))*d + Diagonal(zku ./ (u - xk))*d
-                @printf("%d %10.5e\n", ~isempty(methods(Bk)), ∇²ϕ(zeros(size(xk))))
+                @printf("%d %10.5e\n", ~isempty(methods(Bk)), norm(∇²ϕ(zeros(size(xk)))))
 
             else
                 # ∇²ϕ(d) = Bk*d + Diagonal(zkl ./ (xk - l))*d + Diagonal(zku ./ (u - xk))*d
