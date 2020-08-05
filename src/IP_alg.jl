807452e3d5f68f65b2664a95b4390b93a90e8cb8
rjbaraldi <rbaraldi@uw.edu>
2020-08-04 21:15:46 -0700

    division error debugging

--- src/IP_alg.jl
+++ src/IP_alg.jl
@@ -342,7 +342,8 @@ function IntPt_TR(
             # filestring = string("figs/bpdn/LS_l0_Binf/xcomp", k, ".pdf")
             # savefig(filestring)       
             #Print values
-            k % ptf == 0 && @printf(
+            # k % ptf == 0 && 
+            @printf(
                 "%11d|  %10.5e  %19.5e   %18.5e   %17.5e   %10.5e   %10s   %10.5e   %10s   %10.5e   %10.5e   %10.5e   %10.5e   %10.5e   %10.5e \n",
                 k, μ, kktNorm[1]/kktInit[1],  kktNorm[2]/kktInit[2],  kktNorm[3]/kktInit[3], ρk, x_stat, Δk, TR_stat, α, norm(xk, 2), norm(s, 2), power_iteration(∇²ϕ, randn(size(xk)))[1], fk, ψk(xk))
                 # k, μ, kktNorm[1],  kktNorm[2],  kktNorm[3], ρk, x_stat, Δk, TR_stat, α, norm(xk, 2), norm(s, 2), power_iteration(∇²ϕ, randn(size(xk)))[1], fk, ψk(xk))
