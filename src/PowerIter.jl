d3bb6f13a9569982f9316d099bf52e1f02a078bf
rjbaraldi <rbaraldi@uw.edu>
2020-08-12 16:12:39 -0700

    ok changed it back?

--- src/PowerIter.jl
+++ src/PowerIter.jl
@@ -1,41 +1,40 @@
 export power_iteration
 
-function power_iteration(A, bk; tol=1e-10)
+function power_iteration(A, x; tol=1e-10)
     
-    k = maximum(size(bk))*10000
-    λ, bk = pwrsub(A, bk, tol, k)
+    k = maximum(size(x))*10000
+    λ, x1 = pwrsub(A, x, tol, k)
     
     if λ < 0
         Bk(x) = A(x) - λ*x
-        μ, bk = pwrsub(Bk, bk, tol, k)
-        λ = μ - λ
+        x2 = randn(size(x1))
+        μ, x3 = pwrsub(Bk, x2, tol, k)
+        λ = μ + λ
     end
     
-    return λ, bk
+    return λ, x3
 
 
 end
 
-function pwrsub(A, bk, tol, iters)
+function pwrsub(A, ak, tol, iters)
 
     for i =1:iters
         #this should be matrix multiplication
-        b = A(bk)
+        a = A(ak)
 
         #normalize
-        b = b/norm(b)
-        # μ = (bk'*b)/(bk'*b)
-        # μ = b'*A(bk)
+        a = a/norm(a)
 
         # if abs(μ - μ_im1)<tol 
-        if norm(b - bk)<tol
+        if norm(a - ak)<tol
             break
         end
 
-        bk = b
+        ak = a
 
     end
-    μ = bk'*A(bk)
+    μ = ak'*A(ak)
 
     return μ, bk
 
