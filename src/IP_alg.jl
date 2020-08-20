9b373c211f63fff336681028db47de1056837726
rjbaraldi <rbaraldi@uw.edu>
2020-08-20 14:22:00 -0700

    commented out Gnu show

--- src/IP_alg.jl
+++ src/IP_alg.jl
@@ -316,8 +316,6 @@ function IntPt_TR(
                 # k, μ, kktNorm[1]/kktInit[1],  kktNorm[2]/kktInit[2],  kktNorm[3]/kktInit[3], ρk, x_stat, Δk, TR_stat, α, norm(xk, 2), norm(s, 2), β, fk, ψk(xk))
                 k, μ, kktNorm[1],  kktNorm[2],  kktNorm[3], ρk, x_stat, Δk, TR_stat, α, norm(xk, 2), norm(s, 2), β, fk, ψk(xk))
 
-                @show norm(Gν)
-                @show norm(∇qksj - ∇qk)
             if k % ptf == 0
                 FO_options.optTol = FO_options.optTol * 0.1
             end
--- test_LS_nobarrier.jl
+++ test_LS_nobarrier.jl
@@ -5,89 +5,91 @@ using Plots, Convex, SCS, Printf,LinearAlgebra
 #Here we just try to solve the l2-norm Problem over the l1 trust region
 #######
 # min_x 1/2||Ax - b||^2
-# min_x f(x)
+function LSnobar()
 
-# m,n = 200,100 # this is a under determined system
-compound=1
-m,n = compound*200,compound*512
-A = rand(m,n)
-x0  = rand(n,)
-b0 = A*x0
-b = b0 + 0.5*rand(m,)
-cutoff = 0.0
+    # m,n = 200,100 # this is a under determined system
+    compound=1
+    m,n = compound*200,compound*512
+    A = rand(m,n)
+    x0  = rand(n,)
+    b0 = A*x0
+    b = b0 + 0.5*rand(m,)
+    cutoff = 0.0
 
-function f_obj(x)
-    f = .5*norm(A*x-b)^2
-    g = A'*(A*x - b)
-    # h = A'*A #-> BFGS later
-    h(d) = A'*(A*d)
-    return f, g, h
+    function f_obj(x)
+        f = .5*norm(A*x-b)^2
+        g = A'*(A*x - b)
+        # h = A'*A #-> BFGS later
+        h(d) = A'*(A*d)
+        return f, g, h
+    end
+
+    #SPGSlim version
+    function tr_norm_spg(z,α,σ)
+        return z./max(1, norm(z, 2)/σ)
+    end
+
+    function tr_norm(z,σ, x, Δ)
+        return z./max(1, norm(z, 2)/σ)
+    end
+
+    function h_obj(x)
+        return 0
+    end
+
+    #set all options
+    first_order_options_spgslim = spg_options(;optTol=1.0e-1, progTol=1.0e-10, verbose=0,
+        feasibleInit=true, curvilinear=true, bbType=true, memory=1)
+    first_order_options_proj = s_options(1/eigmax(A'*A);maxIter=1000, verbose=10, optTol=1.0e-3)
+        #need to tighten this because you don't make any progress in the later iterations
+
+
+    # Interior Pt Algorithm
+    parameters_spgslim = IP_struct(f_obj, h_obj;
+        FO_options = first_order_options_spgslim, Rkprox=tr_norm_spg) #defaults to h=0, spgl1/min_confSPG
+    parameters_proj = IP_struct(f_obj, h_obj;
+        s_alg = PG, FO_options = first_order_options_proj, Rkprox=tr_norm)
+    # parameters = IP_struct(f_obj, h_obj;FO_options = first_order_options, Rk=tr_norm) #defaults to h=0, spgl1/min_confSPG
+    options_spgslim = IP_options(; ptf=100, ϵD=1e-5) #print freq, ΔK init, epsC/epsD initialization, maxIter
+    options_proj= IP_options(;ptf=1, ϵD=1e-6)
+
+    #put in your initial guesses
+    xi = ones(n,)/2
+
+    X = Variable(n)
+    problem = minimize(sumsquares(A * X - b))
+    solve!(problem, SCS.Optimizer)
+
+
+    x_spg, k, Fhist_spg, Hhist_spg = IntPt_TR(xi, parameters_spgslim, options_spgslim)
+    x_pr, k, Fhist_pg, Hhist_pg = IntPt_TR(xi, parameters_proj, options_proj)
+
+
+    #print out l2 norm difference and plot the two x values
+    @printf("l2-norm TR (SPGSlim) vs True: %5.5e\n", norm(x_spg - x0))
+    @printf("l2-norm TR (PG) vs True: %5.5e\n", norm(x_pr - x0))
+    @printf("l2-norm CVX vs True: %5.5e\n", norm(X.value - x0))
+    @printf("TR (SPGSlim) vs CVX relative error: %5.5e\n", norm(X.value - x_spg)/norm(X.value))
+    @printf("TR (PG) vs CVX relative error: %5.5e\n", norm(X.value - x_pr)/norm(X.value))
+    plot(x0, xlabel="i^th index", ylabel="x", title="TR vs True x", label="True x")
+    plot!(x_spg, label="tr-spg", marker=2)
+    plot!(x_pr, label="tr-pr", marker=3)
+    plot!(X.value, label="cvx")
+    savefig("figs/ls/xcomp.pdf")
+
+    plot(b0, xlabel="i^th index", ylabel="b", title="TR vs True x", label="True b")
+    plot!(b, label="Observed")
+    plot!(A*x_spg, label="A*x: TR-spg", marker=2)
+    plot!(A*x_pr, label="A*x: TR-pr", marker=3)
+    plot!(A*X.value, label="A*x: CVX")
+    savefig("figs/ls/bcomp.pdf")
+
+    plot(Fhist_spg, xlabel="k^th index", ylabel="Function Value", title="Objective Value History", label="f(x) (SPGSlim)")
+    plot!(Hhist_spg, label="h(x) (SPGSlim)")
+    plot!(Fhist_spg+ Hhist_spg, label="f+h (SPGSlim)")
+    plot!(Hhist_pg, label="h(x) (Prox-grad)")
+    plot!(Fhist_pg, label="f(x) (Prox-grad)")
+    plot!(Fhist_pg+ Hhist_pg, label="f+h (Prox-grad)")
+    savefig("figs/ls/objhist.pdf")
+
 end
-
-#SPGSlim version
-function tr_norm_spg(z,α,σ)
-    return z./max(1, norm(z, 2)/σ)
-end
-
-function tr_norm(z,σ, x, Δ)
-    return z./max(1, norm(z, 2)/σ)
-end
-
-function h_obj(x)
-    return 0
-end
-
-#set all options
-first_order_options_spgslim = spg_options(;optTol=1.0e-1, progTol=1.0e-10, verbose=0,
-    feasibleInit=true, curvilinear=true, bbType=true, memory=1)
-first_order_options_proj = s_options(1/eigmax(A'*A);maxIter=1000, verbose=0, optTol=1.0e-3)
-    #need to tighten this because you don't make any progress in the later iterations
-
-
-# Interior Pt Algorithm
-parameters_spgslim = IP_struct(f_obj, h_obj;
-    FO_options = first_order_options_spgslim, Rkprox=tr_norm_spg) #defaults to h=0, spgl1/min_confSPG
-parameters_proj = IP_struct(f_obj, h_obj;
-    s_alg = PG, FO_options = first_order_options_proj, Rkprox=tr_norm)
-# parameters = IP_struct(f_obj, h_obj;FO_options = first_order_options, Rk=tr_norm) #defaults to h=0, spgl1/min_confSPG
-options_spgslim = IP_options(; ptf=100, ϵD=1e-5) #print freq, ΔK init, epsC/epsD initialization, maxIter
-options_proj= IP_options(;ptf=1, ϵD=1e-6)
-
-#put in your initial guesses
-xi = ones(n,)/2
-
-X = Variable(n)
-problem = minimize(sumsquares(A * X - b))
-solve!(problem, SCS.Optimizer)
-
-
-x_spg, k, Fhist_spg, Hhist_spg = IntPt_TR(xi, parameters_spgslim, options_spgslim)
-x_pr, k, Fhist_pg, Hhist_pg = IntPt_TR(xi, parameters_proj, options_proj)
-
-
-#print out l2 norm difference and plot the two x values
-@printf("l2-norm TR (SPGSlim) vs True: %5.5e\n", norm(x_spg - x0))
-@printf("l2-norm TR (PG) vs True: %5.5e\n", norm(x_pr - x0))
-@printf("l2-norm CVX vs True: %5.5e\n", norm(X.value - x0))
-@printf("TR (SPGSlim) vs CVX relative error: %5.5e\n", norm(X.value - x_spg)/norm(X.value))
-@printf("TR (PG) vs CVX relative error: %5.5e\n", norm(X.value - x_pr)/norm(X.value))
-plot(x0, xlabel="i^th index", ylabel="x", title="TR vs True x", label="True x")
-plot!(x_spg, label="tr-spg", marker=2)
-plot!(x_pr, label="tr-pr", marker=3)
-plot!(X.value, label="cvx")
-savefig("figs/ls/xcomp.pdf")
-
-plot(b0, xlabel="i^th index", ylabel="b", title="TR vs True x", label="True b")
-plot!(b, label="Observed")
-plot!(A*x_spg, label="A*x: TR-spg", marker=2)
-plot!(A*x_pr, label="A*x: TR-pr", marker=3)
-plot!(A*X.value, label="A*x: CVX")
-savefig("figs/ls/bcomp.pdf")
-
-plot(Fhist_spg, xlabel="k^th index", ylabel="Function Value", title="Objective Value History", label="f(x) (SPGSlim)")
-plot!(Hhist_spg, label="h(x) (SPGSlim)")
-plot!(Fhist_spg+ Hhist_spg, label="f+h (SPGSlim)")
-plot!(Hhist_pg, label="h(x) (Prox-grad)")
-plot!(Fhist_pg, label="f(x) (Prox-grad)")
-plot!(Fhist_pg+ Hhist_pg, label="f+h (Prox-grad)")
-savefig("figs/ls/objhist.pdf")
