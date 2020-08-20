8c6d8398e38aa080f47e2df3dd82a79caac4c7bc
rjbaraldi <rbaraldi@uw.edu>
2020-08-20 11:59:49 -0700

    slight changes to IP_alg to see what PG is outputting

--- src/IP_alg.jl
+++ src/IP_alg.jl
@@ -233,14 +233,9 @@ function IntPt_TR(
 
             #allow for different cases if the objective is simple -> generalize this later maybe? 
             objInner(d) = [0.5*(d'*∇²qk(d)) + ∇qk'*d + qk, ∇²qk(d) + ∇qk] #(mkB, ∇mkB)
+            s⁻ = zeros(size(xk))
 
-            # if simple == 1
-            #     funProj(d) = Rkprox(d, 1.0, Δk) #projects onto ball of radius Δk, weights of 1.0
-            #     s⁻ = zeros(size(xk))
-            #     (s, fsave, funEvals) = s_alg(objInner, s⁻, funProj, FO_options)
-            #     # Gν = (s⁻ - s)/ν = 1/(1/β)(-s) = -(s)β
-            #     Gν = -s * power_iteration(∇²qk,randn(size(xk)))[1]    #this isn't quite right for spg_minconf since you technically need the previous g output
-            # else
+            # if simple == 0
                 FO_options.β = power_iteration(∇²qk,randn(size(xk)))[1]
                 # FO_options.Bk = ∇²qk
                 # FO_options.∇fk = ∇qk
@@ -250,11 +245,21 @@ function IntPt_TR(
                 if simple == 2
                     FO_options.λ = Δk * FO_options.β
                 end
-                funProj(d, σ)= Rkprox(d, σ, xk, Δk)
+                funProj(d, λν)= Rkprox(d, λν, xk, Δk)
+
+
                 (s, s⁻, fsave, funEvals) = s_alg(objInner, s⁻, funProj, FO_options)
 
                 # Gν = (s⁻ - s) * FO_options.β
+                @show norm((s⁻ - s))
                 Gν = (- s) * FO_options.β
+
+
+            # else 
+            #     funProj(d) = Rkprox(d, 1.0, Δk) #projects onto ball of radius Δk, weights of 1.0
+            #     (s, fsave, funEvals) = s_alg(objInner, s⁻, funProj, FO_options)
+            #     # Gν = (s⁻ - s)/ν = 1/(1/β)(-s) = -(s)β
+            #     Gν = -s * power_iteration(∇²qk,randn(size(xk)))[1]    #this isn't quite right for spg_minconf since you technically need the previous g output
             # end
 
             #compute qksj for the previous iterate 
