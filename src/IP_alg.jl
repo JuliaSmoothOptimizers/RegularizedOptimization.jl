bb0d43aa41c0e2aa7f5baf72ae746fcd79d60e37
rjbaraldi <rbaraldi@uw.edu>
2020-08-20 14:50:47 -0700

    added H changes to hessdeter

--- src/IP_alg.jl
+++ src/IP_alg.jl
@@ -290,7 +290,7 @@ function IntPt_TR(
 
             Fsmth_out = f_obj(xk)
 
-            (fk, ∇fk, H) = hessdeter(Fsmth_out, xk, k_i,typeof(FO_options), s, ∇fk⁻)
+            (fk, ∇fk, H) = hessdeter(Fsmth_out, xk, k_i,typeof(FO_options), s, ∇fk⁻, H)
 
             #update qk with new direction
             qk = fk - μ * sum(log.(xk - l)) - μ * sum(log.(u - xk))
@@ -331,15 +331,15 @@ function IntPt_TR(
 end
 
 
-function hessdeter(fsmth_output, x, kk, opttype, sdes=zeros(size(x)), gradf_prev=zeros(size(x)))
+function hessdeter(fsmth_output, x, kk, opttype, sdes=zeros(size(x)), gradf_prev=zeros(size(x)), Hess=I(size(x,1)))
     if length(fsmth_output)==3 #get regular number of outputs
         (f, ∇f, Hess) = fsmth_output
     elseif length(fsmth_output)==2 && kk==0
         (f, ∇f) = fsmth_output #if 2 outputs and if minconf_spg is in play; simple and β should stay in scope 
         if opttype==typeof(spg_options())
-            Hess = I(size(x, 1))
+            Hess = Hess
         else
-            Hess = β*I(size(x,1))
+            Hess = β*Hess
         end
     elseif length(fsmth_output)==2 && kk>0 #if 2 outputs and you're past the first iterate 
         (f, ∇f) = fsmth_output
