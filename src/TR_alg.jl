c7c9d488aef762232bc8da2b78bc2f65aaa78e8a
rjbaraldi <rbaraldi@uw.edu>
2021-04-24 23:23:44 -0400

    more grad! testing

--- src/TR_alg.jl
+++ src/TR_alg.jl
@@ -173,7 +173,7 @@ xk = deepcopy(f.meta.x0)
 
 			#update gradient & hessian 
 			if !optimal 
-				 grad!(f, xk, ∇fk)
+				  ∇fk = grad!(f, xk, ∇fk)
 				if quasiNewtTest
 					push!(f, s, ∇fk - ∇fk⁻)
 					Bk = hess_op(f, xk)
