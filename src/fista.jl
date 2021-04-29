a7ddfff0b103b7de467a36abcdc3a978accf4ab0
rjbaraldi <rbaraldi@uw.edu>
2021-04-28 23:09:20 -0500

    reformatting of PG, will need to decide on arguments

--- src/fista.jl
+++ src/fista.jl
@@ -1,151 +1,145 @@
 export FISTA, FISTAD
 using Printf
 """
-	FISTA for
-	min_x ϕ(x) = f(x) + g(x), with f(x) cvx and β-smooth, g(x) closed cvx
+  FISTA for
+  min_x ϕ(x) = f(x) + g(x), with f(x) cvx and β-smooth, g(x) closed cvx
 
-	Input:
-		Fcn: function handle that returns f(x) and ∇f(x)
-		Gcn: function handle that returns g(x)
-		s: initial point
-		proxG: function handle that calculates prox_{νg}
-		options: see descentopts.jl
-	Output:
-		s⁺: s update
-		s : s^(k-1)
-		his : function history
-		feval : number of function evals (total objective)
+  Input:
+    Fcn: function handle that returns f(x) and ∇f(x)
+    Gcn: function handle that returns g(x)
+    s: initial point
+    proxG: function handle that calculates prox_{νg}
+    options: see descentopts.jl
+  Output:
+    s⁺: s update
+    s : s^(k-1)
+    his : function history
+    feval : number of function evals (total objective)
 """
-function FISTA(Fcn, Gcn, s,  proxG, options)
-	ε=options.optTol
-	max_iter=options.maxIter
-	restart = options.restart
-	ν = options.ν
-	λ = options.λ
-	if options.verbose==0
-		print_freq = Inf
-	elseif options.verbose==1
-		print_freq = round(max_iter/10)
-	elseif options.verbose==2
-		print_freq = round(max_iter/100)
-	else
-		print_freq = 1
-	end
+function FISTA(GradFcn, Gcn, s,  options)
+  ε=options.optTol
+  max_iter=options.maxIter
+  restart = options.restart
+  ν = options.ν
 
-	#get types
-	T = eltype(s)
-	R = real(T)
+  if options.verbose==0
+    print_freq = Inf
+  elseif options.verbose==1
+    print_freq = round(max_iter/10)
+  elseif options.verbose==2
+    print_freq = round(max_iter/100)
+  else
+    print_freq = 1
+  end
 
-	#Problem Initialize
-	m = length(s)
-	y = deepcopy(s)
-	s⁺ = zeros(T, m)
-	#initialize parameters
-	t = R(1.0)
-	# Iteration set up
-	k = 1
-	err = R(100.0)
-	his = zeros(max_iter)
+  #Problem Initialize
+  y = deepcopy(s)
+  s⁺ = zero(s)
+  #initialize parameters
+  t = 1.0
+  # Iteration set up
+  k = 0
+  feval = 1
 
-	#do iterations
-	f, g= Fcn(y)[1:2]
-	feval = 1
-	while err >= ε && k<max_iter
-		copy!(s,s⁺)
-		his[k] = f + Gcn(y)
-		s⁺ = proxG(y - ν*g, ν*λ)
+  #do iterations
+  g = GradFcn(s⁺) #objInner/ quadratic model
 
-		#update step
-		t⁻ = t
-		# t = 0.5*(1.0 + sqrt(1.0+4.0*t⁻^2))
-		t = R(0.5)*(R(1.0) + sqrt(R(1.0)+R(4.0)*t⁻^2))
+  optimal = false
+  tired = k ≥ max_iter
+  
+  while !(optimal || tired)
 
-		#update y
-		y = s⁺ + ((t⁻ - R(1.0))/t)*(s⁺-s)
+    copy!(s,s⁺)
+    gold = g 
+    s⁺ = prox(Gcn, s - ν*g, ν) 
 
-		#check convergence
-		err = norm(s - s⁺)
+    #update step
+    t⁻ = t
+    t = 0.5*(1.0 + sqrt(1.0+4.0*t⁻^2))
 
-		#update parameters
-		f, g= Fcn(y)[1:2]
+    #update y
+    y .= s⁺ .+ ((t⁻ - 1.0)/t) .* (s⁺.- s)
 
-		feval+=1
-		k+=1
-	end
-	return s⁺, his[1:k-1], feval
+    g = GradFcn(s⁺)
+
+    feval+=1
+    k+=1
+    err = norm(g-gold - (s⁺-s)/ν)
+    optimal = err < ε 
+    tired = k ≥ max_iter
+  end
+  return s⁺, feval
 
 end
 
 #enforces strict descent  for FISTA 
-function FISTAD(Fcn, Gcn, s,  proxG, options)
-	ε=options.optTol
-	max_iter=options.maxIter
-	restart = options.restart
-	ν = options.ν
-	λ = options.λ
-	if options.verbose==0
-		print_freq = Inf
-	elseif options.verbose==1
-		print_freq = round(max_iter/10)
-	elseif options.verbose==2
-		print_freq = round(max_iter/100)
-	else
-		print_freq = 1
-	end
+function FISTAD(GradFcn, Gcn, s, options)
+  ε=options.optTol
+  max_iter=options.maxIter
+  restart = options.restart
+  ν = options.ν
 
-	#get types
-	T = eltype(s)
-	R = real(T)
+  if options.verbose==0
+    print_freq = Inf
+  elseif options.verbose==1
+    print_freq = round(max_iter/10)
+  elseif options.verbose==2
+    print_freq = round(max_iter/100)
+  else
+    print_freq = 1
+  end
 
-	#Problem Initialize
-	m = length(s)
-	v = deepcopy(s)
-	s⁺ = zeros(T, m)
-	#initialize parameters
-	t = R(1.0)
-	# Iteration set up
-	k = 1
-	err = R(100.0)
-	his = zeros(max_iter)
+  #Problem Initialize
+  v = deepcopy(s)
+  s⁺ = zero(s)
+  #initialize parameters
+  t = 1.0
+  # Iteration set up
+  k = 0
+  feval = 1
 
-	#do iterations
-	y = (R(1.0)-t)*s + t*v
-	f, g= Fcn(y)[1:2] 
+  #do iterations
+  y = (1.0-t)*s + t*v
+  g= GradFcn(y) 
 
-	feval = 1
-	while err >= ε && k<max_iter
-		copy!(s,s⁺)
-		his[k] = f + Gcn(y)
+  optimal = false
+  tired = k ≥ max_iter
+  
+  while !(optimal || tired)
 
-		#complete prox step 
-		u = proxG(y - ν*g, ν*λ)
+    copy!(s,s⁺)
+    gold = g 
 
-		if Fcn(u)[1] ≤ f
-			s⁺ = u
-		else
-			s⁺ = s
-		end
+    #complete prox step 
+    u = prox(Gcn, y - ν*g, ν)
 
-		#update step
-		# t⁻ = t
-		# t = R(0.5)*(R(1.0) + sqrt(R(1.0)+R(4.0)*t⁻^2))
-		t = R(2/(k + 1))
+    if Fcn(u)[1] ≤ f #this does not work 
+      s⁺ = u
+    else
+      s⁺ = s
+    end
 
-		#update y
-		# v = s⁺ + ((t⁻ - R(1.0))/t)*(s⁺-s)
-		v = s⁺ + (R(1.0)/t)*(u - s⁺)
-		y = (R(1.0)-t)*s⁺ + t*v #I think this shold be s⁺ since it's at the end of the loop 
+    #update step
+    # t⁻ = t
+    # t = R(0.5)*(R(1.0) + sqrt(R(1.0)+R(4.0)*t⁻^2))
+    t = 2/(k + 1)
 
-		#check convergence
-		err = norm(s - s⁺)
+    #update y
+    # v = s⁺ + ((t⁻ - R(1.0))/t)*(s⁺-s)
+    v = s⁺ + (1.0/t)*(u - s⁺)
+    y = (1.0-t)*s⁺ + t*v #I think this shold be s⁺ since it's at the end of the loop 
 
+    #update parameters
+    g = GradFcn(y)
 
-		#update parameters
-		f, g= Fcn(y)[1:2]
+    #check convergence
+    err = norm(s - s⁺)
+    feval+=1
+    k+=1
+    optimal = err < ε 
+    tired = k ≥ max_iter
 
-		feval+=1
-		k+=1
-	end
-	return s⁺, his[1:k-1], feval
+  end
+  return s⁺, feval
 
 end
--- src/proxgrad.jl
+++ src/proxgrad.jl
@@ -1,4 +1,4 @@
-export PG, PGLnsch, PGΔ, PGE, PGnew
+export PG, PGLnsch, PGΔ, PGE
 
 using Printf
 """
@@ -17,241 +17,7 @@ using Printf
         his : function history
         feval : number of function evals (total objective )
 """
-function PG(Fcn, Gcn, s,  proxG, options)
-
-    ε=options.optTol
-    max_iter=options.maxIter
-
-    if options.verbose==0
-        print_freq = Inf
-    elseif options.verbose==1
-        print_freq = round(max_iter/10)
-    elseif options.verbose==2
-        print_freq = round(max_iter/100)
-    else
-        print_freq = 1
-    end
-    #Problem Initialize
-    m = length(s)
-    ν = options.ν
-    λ = options.λ
-    k = 1
-    err = 100.0
-    his = zeros(max_iter)
-    s⁺ = deepcopy(s)
-
-    # Iteration set up
-    f, g, _ = Fcn(s⁺) #objInner/ quadratic model
-    fstart = f
-    feval = 1
-
-    #do iterations
-    while err >= ε && k<max_iter #another stopping criteria abs(f - fstart)>TOL*||Δf(s1)||
-
-        his[k] = f + Gcn(s⁺)*λ #Gcn = h(x)
-        #sheet on which to freq
-        
-        gold = g
-        s = s⁺
-
-        #prox step
-        s⁺ = proxG(s - ν*g, λ*ν) #combination regularizer + TR
-        # update function info
-        f, g = Fcn(s⁺)[1:2]
-        feval+=1
-
-        # err = norm((s-s⁺)/ν) #stopping criteria
-        err = norm(g-gold - (s⁺-s)/ν) #(Bk - ν^-1I)(s⁺ -s ) ----> equation 17 in paper 
-        # err = norm((s-s⁺)/ν - gold) #equation 16 in paper
-    
-        k+=1
-    end
-    return s⁺, his[1:k-1], feval
-end
-
-function PGLnsch(Fcn,Gcn, s,  proxG, options)
-
-    ε=options.optTol
-    max_iter=options.maxIter
-
-    if options.verbose==0
-        print_freq = Inf
-    elseif options.verbose==1
-        print_freq = round(max_iter/10)
-    elseif options.verbose==2
-        print_freq = round(max_iter/100)
-    else
-        print_freq = 1
-    end
-    
-    T = eltype(s)
-    R = real(T)
-    
-    
-    #Problem Initialize
-    m = length(s)
-    p = options.p
-    # ν = R(1.0)
-    ν = options.ν
-    λ = options.λ
-    k = 1
-    err = 100.0
-    his = zeros(max_iter)
-    s⁺ = deepcopy(s)
-
-
-    # Iteration set up
-    f, g = Fcn(s⁺)[1:2]
-    feval = 1
-    #do iterations
-    while err >= ε && k<max_iter
-        s = s⁺
-        his[k] = f + Gcn(s⁺)
-        #prox step
-        s⁺ = proxG(s - ν*g, λ*ν)
-        #linesearch
-        while Fcn(s⁺)[1] ≥ f + g'*(s⁺ - s) + 1/(ν*2)*norm(s⁺ - s)^2
-            ν *= p*ν
-            s⁺ = proxG(s - ν*g, λ*ν)
-            feval+=1
-        end
-        # update function info
-        ν = options.ν
-        f, g = Fcn(s⁺)[1:2]
-
-        feval+=1
-        err = norm(s-s⁺)
-        k+=1
-
-    end
-    return s⁺, his[1:k-1], feval
-end
-
-
-
-function PGΔ(Fcn, Gcn, s,  proxG, options)
-
-    ε=options.optTol
-    max_iter=options.maxIter
-
-    if options.verbose==0
-        print_freq = Inf
-    elseif options.verbose==1
-        print_freq = round(max_iter/10)
-    elseif options.verbose==2
-        print_freq = round(max_iter/100)
-    else
-        print_freq = 1
-    end
-    #Problem Initialize
-    m = length(s)
-    ν = options.ν
-    λ = options.λ
-
-
-    p = options.p 
-    FcnDec = options.FcnDec
-
-
-    k = 1
-    err = 100.0
-    his = zeros(max_iter)
-    s⁺ = deepcopy(s)
-
-    # Iteration set up
-    f, g = Fcn(s⁺)[1:2] #objInner/ quadratic model
-    his[k] = f + Gcn(s⁺)*λ #Gcn = h(x)
-    DiffFcn = 0.0 
-    feval = 1
-    #do iterations
-    while err >= ε && k<max_iter && abs(DiffFcn)<p*norm(FcnDec) #another stopping criteria abs(f - fstart)>TOL*||Δf(s1)||
-
-        #sheet on which to freq
-
-        gold = g
-        s = s⁺
-
-        #prox step
-        s⁺ = proxG(s - ν*g, λ*ν) #combination regularizer + TR
-        # update function info
-        f, g = Fcn(s⁺)[1:2]
-        feval+=1
-
-        # err = norm((s-s⁺)/ν) #stopping criteria
-        err = norm(g-gold - (s⁺-s)/ν) #(Bk - ν^-1I)(s⁺ -s ) ----> equation 17 in paper 
-        # err = norm((s-s⁺)/ν - gold) #equation 16 in paper
-        k+=1
-
-        
-        his[k] = f + Gcn(s⁺)*λ #Gcn = h(x)
-        DiffFcn = his[k-1] - his[k]
-    end
-    return s⁺, his[1:k-1], feval
-end
-
-
-
-function PGE(Fcn, Gcn, s,  proxG, options)
-
-    ε=options.optTol
-    max_iter=options.maxIter
-
-    if options.verbose==0
-        print_freq = Inf
-    elseif options.verbose==1
-        print_freq = round(max_iter/10)
-    elseif options.verbose==2
-        print_freq = round(max_iter/100)
-    else
-        print_freq = 1
-    end
-    #Problem Initialize
-    m = length(s)
-    ν = options.ν
-    λ = options.λ
-
-
-    p = options.p 
-    FcnDec = options.FcnDec
-
-
-    k = 1
-    err = 100.0
-    his = zeros(max_iter)
-    s⁺ = deepcopy(s)
-
-    # Iteration set up
-    f, g, Bk = Fcn(s⁺) #objInner/ quadratic model
-    his[k] = f + Gcn(s⁺)*λ #Gcn = h(x)
-    DiffFcn = 0.0 
-    feval = 1
-    #do iterations
-    while err >= ε && k<max_iter && abs(DiffFcn)<p*norm(FcnDec) #another stopping criteria abs(f - fstart)>TOL*||Δf(s1)||
-
-        #sheet on which to freq
-        gold = g
-        s = s⁺
-
-        ν = min(g'*g/(g'*Bk*g), ν)
-        #prox step
-        s⁺ = proxG(s - ν*g, λ*ν) #combination regularizer + TR
-        # update function info
-        f, g, Bk = Fcn(s⁺)
-        feval+=1
-
-        # err = norm((s-s⁺)/ν) #stopping criteria
-        err = norm(g-gold - (s⁺-s)/ν) #(Bk - ν^-1I)(s⁺ -s ) ----> equation 17 in paper 
-        # err = norm((s-s⁺)/ν - gold) #equation 16 in paper
-        k+=1
-
-        
-        his[k] = f + Gcn(s⁺)*λ #Gcn = h(x)
-        DiffFcn = his[k-1] - his[k]
-    end
-    return s⁺, his[1:k-1], feval
-end
-
-function PGnew(GradFcn, Gcn, s, options)
+function PG(GradFcn, Gcn, s, options)
 
   ε=options.optTol
   max_iter=options.maxIter
@@ -265,40 +31,210 @@ function PGnew(GradFcn, Gcn, s, options)
   else
     print_freq = 1
   end
+
   #Problem Initialize
-  m = length(s)
   ν = options.ν
-  k = 1
-  err = 100.0
-  his = zeros(max_iter)
   s⁺ = deepcopy(s)
 
   # Iteration set up
   g = GradFcn(s⁺) #objInner/ quadratic model
   feval = 1
-  k % print_freq == 0 && @info @sprintf "%4d ‖xᵏ⁺¹ - xᵏ‖=%1.5e ν = %1.5e" k err ν
+  k = 0
 
   #do iterations
-  while err >= ε && k<max_iter #another stopping criteria abs(f - fstart)>TOL*||Δf(s1)||
+  optimal = false
+  tired = k ≥ max_iter
 
-    #sheet on which to freq
-    
+  while !(optimal || tired)
+
     gold = g
     s = s⁺
 
-    #prox step
-    s⁺ = prox(Gcn, s - ν*g, ν) #combination regularizer + TR
+    s⁺ = prox(Gcn, s - ν*g, ν) 
+
+    g = GradFcn(s⁺)
+
+    feval+=1
+    k+=1
+    err = norm(g-gold - (s⁺-s)/ν)
+    optimal = err < ε 
+    tired = k ≥ max_iter
+
+    k % print_freq == 0 && @info @sprintf "%4d ‖xᵏ⁺¹ - xᵏ‖=%1.5e ν = %1.5e" k err ν
+ 
+  end
+  return s⁺, feval
+end
+
+function PGΔ(GradFcn, Gcn, s, options)
+
+  ε=options.optTol
+  max_iter=options.maxIter
+
+  if options.verbose==0
+      print_freq = Inf
+  elseif options.verbose==1
+      print_freq = round(max_iter/10)
+  elseif options.verbose==2
+      print_freq = round(max_iter/100)
+  else
+      print_freq = 1
+  end
+  #Problem Initialize
+  ν = options.ν
+  p = options.p 
+  FcnDec = options.FcnDec
+
+  k = 1
+  feval = 1
+  s⁺ = deepcopy(s)
+
+  # Iteration set up
+  g = GradFcn(s⁺) #objInner/ quadratic model
+  @info @sprintf "%4d ‖xᵏ⁺¹ - xᵏ‖=%1.5e ν = %1.5e" k err ν
+
+  #do iterations
+  optimal = false
+  FD = false
+  tired = k ≥ max_iter
+
+  while !(optimal || tired || FD)
+
+    gold = g
+    s = s⁺
+
+    s⁺ = prox(Gcn, s - ν*g, ν)
     # update function info
     g = GradFcn(s⁺)
-    feval+=1
 
-    # err = norm((s-s⁺)/ν) #stopping criteria
-    err = norm(g-gold - (s⁺-s)/ν) #(Bk - ν^-1I)(s⁺ -s ) ----> equation 17 in paper 
-    # err = norm((s-s⁺)/ν - gold) #equation 16 in paper
+    feval+=1
     k+=1
+    err = norm(g-gold - (s⁺-s)/ν)
+    optimal =  err < ε
+    tired = k ≥ max_iter
+
     k % print_freq == 0 && @info @sprintf "%4d ‖xᵏ⁺¹ - xᵏ‖=%1.5e ν = %1.5e" k err ν
 
-      
+    DiffFcn = his[k-1] - his[k] # these do not work 
+    FD = abs(DiffFcn)<p*norm(FcnDec)
+
+  end
+  return s⁺, feval
+end
+
+
+
+function PGE(Fcn, Gcn, s, options)
+
+  ε=options.optTol
+  max_iter=options.maxIter
+
+  if options.verbose==0
+      print_freq = Inf
+  elseif options.verbose==1
+      print_freq = round(max_iter/10)
+  elseif options.verbose==2
+      print_freq = round(max_iter/100)
+  else
+      print_freq = 1
+  end
+  #Problem Initialize
+  ν = options.ν
+  p = options.p 
+  FcnDec = options.FcnDec
+
+  k = 1
+  feval = 1
+  s⁺ = deepcopy(s)
+
+  # Iteration set up
+  g = GradFcn(s⁺) #objInner/ quadratic model
+
+  #do iterations
+  FD = false 
+  optimal = false
+  tired = k ≥ max_iter 
+  
+  #do iterations
+  while !(optimal || tired || FD)
+
+    gold = g
+    s = s⁺
+
+    ν = min(g'*g/(g'*Bk*g), ν) #no BK, will not work 
+    #prox step
+    s⁺ = prox(Gcn, s - ν*g, ν)
+    # update function info
+    g = GradFcn(s⁺)
+
+    feval+=1
+    k+=1
+    err = norm(g-gold - (s⁺-s)/ν)
+    optimal =  err < ε
+    tired = k ≥ max_iter
+
+    k % print_freq == 0 && @info @sprintf "%4d ‖xᵏ⁺¹ - xᵏ‖=%1.5e ν = %1.5e" k err ν
+
+    
+    his[k] = f + Gcn(s⁺)*λ # will not work 
+    DiffFcn = his[k-1] - his[k]
+  end
+  return s⁺, his[1:k-1], feval
+end
+
+function PGLnsch(GradFcn, Gcn, s, options)
+
+  ε=options.optTol
+  max_iter=options.maxIter
+
+  if options.verbose==0
+      print_freq = Inf
+  elseif options.verbose==1
+      print_freq = round(max_iter/10)
+  elseif options.verbose==2
+      print_freq = round(max_iter/100)
+  else
+      print_freq = 1
+  end
+  
+  #Problem Initialize
+  p = options.p
+  ν₀ = options.ν
+  k = 1
+  s⁺ = deepcopy(s)
+
+  # Iteration set up
+  feval = 1
+  g = GradFcn(s⁺) #objInner/ quadratic model
+
+  #do iterations
+  optimal = false
+  tired = k ≥ max_iter
+
+  while !(optimal || tired)
+
+    gold = g
+    s = s⁺
+
+    s⁺ = prox(Gcn, s - ν*g, ν)
+    #linesearch but we don't pass in Fcn? 
+    while Fcn(s⁺) ≥ f + g'*(s⁺ - s) + 1/(ν*2)*norm(s⁺ - s)^2
+        ν *= p*ν
+        s⁺ = prox(Gcn, s - ν*g, ν)
+        feval+=1
+    end
+    # update function info
+    g = GradFcn(s⁺) #objInner/ quadratic model
+
+    feval+=1
+    k+=1
+    err = norm(g-gold - (s⁺-s)/ν)
+    optimal = err < ε 
+    tired = k ≥ max_iter
+
+    k % print_freq == 0 && @info @sprintf "%4d ‖xᵏ⁺¹ - xᵏ‖=%1.5e ν = %1.5e" k err ν
+    ν = ν₀
+
   end
   return s⁺, feval
 end
