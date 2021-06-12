2bdf5a77952cacf22de7e82db1516015c6943caa
rjbaraldi <rbaraldi@uw.edu>
2021-06-12 11:12:58 -0700

    optTol -> epsilon

--- src/descentopts.jl
+++ src/descentopts.jl
@@ -4,14 +4,14 @@ export s_params, TRNCoptions, SmoothObj#, FObj, HObj
 mutable struct s_params
   ν
   λ
-  optTol
+  ϵ
   maxIter
   verbose
   p
   FcnDec
 
-  function s_params(ν, λ; optTol=1f-6, maxIter=10000, verbose=0, p=1.1, FcnDec=1e10)
-    return new(ν,λ, optTol, maxIter, verbose, p, FcnDec)
+  function s_params(ν, λ; ϵ=1f-6, maxIter=10000, verbose=0, p=1.1, FcnDec=1e10)
+    return new(ν,λ, ϵ, maxIter, verbose, p, FcnDec)
   end
 end
 
