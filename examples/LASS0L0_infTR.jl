2c2844ee98e926416e8dc838ee14205d2712ec32
rjbaraldi <rbaraldi@uw.edu>
2021-06-12 12:07:15 -0700

    working L0 and L1 with Binf bpdn examples. PG & QR

--- examples/LASS0L0_infTR.jl
+++ examples/LASS0L0_infTR.jl
@@ -32,35 +32,27 @@ function L0BInf()
     return g
   end
 
-  β = opnorm(A)^2
-  # ϕ = LSR1Model(SmoothObj((x) -> .5*norm(A*x - b)^2, gradF!, xi))
-  ϕ = ADNLPModel((x) -> .5*norm(A*x - b)^2, xi) # this is slower
-  # ϕ = ADNLSModel((x)-> A*x - b, xi, m)
+  ϕ = LSR1Model(SmoothObj((x) -> .5*norm(A*x - b)^2, gradF!, xi))
   h = NormL0(λ)
+  χ=NormLinf(1.0)
 
-  #set options for inner algorithm - only requires ||Bk|| norm guess to start (and λ but that is updated in TR)
-  #verbosity is levels: 0 = nothing, 1 -> maxIter % 10, 2 = maxIter % 100, 3+ -> print all 
- #1/||Bk|| for exact Bk = A'*A
-  Doptions=s_params(1/β, λ; verbose=0, optTol=1e-16)
+  ν = opnorm(A)^2 #1/||Bk|| for exact Bk = A'*A
 
-
-  ε = 1e-6
-  methods = TRNCmethods(; FO_options = Doptions, s_alg=PG, χ=NormLinf(1.0))
-  parameters = TRNCparams(;β = 1e16, ϵ=ε, verbose = 10)
-
+  ϵ = 1e-6 
+  parameters = TRNCoptions(;ν = 1.0,  β = 1e16, ϵ=ϵ, verbose = 10)
 
   #input NLP, h, parameters, options 
-  xtr, k, Fhist, Hhist, Comp_pg = TRalg(ϕ, h, methods, parameters)
+  xtr, k, Fhist, Hhist, Comp_pg = TR(ϕ, h, χ, parameters; s_alg = QRalg)
   # x_pr, k, Fhist, Hhist, Comp_pg = LMTR(ϕ, h, methods, parameters)
 
-  paramsQR = TRNCparams(; σk = 1/β, ϵ=ε, verbose = 10) #options, such as printing (same as above), tolerance, γ, σ, τ, w/e
-  # xi .= 0 
+  xi .= 0 
+  reset!(ϕ)
   
   #input initial guess
-  # xqr, kqr, Fhistqr, Hhistqr, Comp_pgqr = QRalg(ϕ, h, methods, paramsQR)
+  xqr, kqr, Fhistqr, Hhistqr, Comp_pgqr = QRalg(ϕ, h, parameters; x0 = xi)
 
   @info "TR relative error" norm(xtr - x0) / norm(x0)
-  # @info "QR relative error" norm(xqr - x0) / norm(x0)
+  @info "QR relative error" norm(xqr - x0) / norm(x0)
   @info "monotonicity" findall(>(0), diff(Fhist+Hhist))
 
 end
