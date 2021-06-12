e2507fcfb848420f7d4cfcd3c4378ca55de7e27f
rjbaraldi <rbaraldi@uw.edu>
2021-06-12 11:12:58 -0700

    optTol -> epsilon

--- src/TR_alg.jl
+++ src/TR_alg.jl
@@ -137,7 +137,7 @@ function TR(
       @info "TR: terminating with ξ1 = $(ξ1)"
       continue
     end
-    FO_options.optTol = k == 1 ? 1.0e-5 : max(ϵ, min(.01, sqrt(ξ1)) * ξ1)
+    FO_options.ϵ = k == 1 ? 1.0e-5 : max(ϵ, min(.01, sqrt(ξ1)) * ξ1)
     FO_options.ν = 1 / νInv
     set_radius!(ψ, min(β * χ(s1), Δk))
     s, funEvals, _, _, _ = s_alg(φ, ∇φ, ψ, s1, FO_options)
