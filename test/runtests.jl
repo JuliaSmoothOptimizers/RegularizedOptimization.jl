fa776913bf64e0482a19b8a207230d642a11ba91
Dominique Orban <dominique.orban@gmail.com>
2021-08-12 16:12:03 -0400

    unit tests for R2 and TR

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,24 +1,61 @@
-using Test, TRNC
-using LinearAlgebra, Random
+using LinearAlgebra, Random, Test
+using ProximalOperators
+using NLPModels, NLPModelsModifiers, RegularizedProblems, TRNC
 
-include("test_bpdn_nobarrier_tr.jl")
+const global compound = 1
+const global nz = 10 * compound
+const global options = TRNCoptions(ν = 1.0,  β = 1e16, ϵ = 1e-6, verbose = 0)
+const global bpdn, sol = bpdn_model(compound)
+const global λ = norm(grad(bpdn, zeros(bpdn.meta.nvar)), Inf) / 10
 
-Random.seed!(0)
-
-@testset "TRNC" begin
-	@testset "Descent Methods" begin
-		# include("test_proxalgs.jl")
-	end
-
-	@testset "Hard Prox Computations" begin 
-		# include("test_hardprox.jl")
-	end
-
-	@testset "LASSO" begin
-		include("test_tr.jl")
-	end
-
-	# @testset "Various Utilities" begin 
-	#   include("test_utilities.jl")
-	# end
+for (mod, mod_name) ∈ ((x -> x, "exact"), (LSR1Model, "lsr1"), (LBFGSModel, "lbfgs"))
+  for (h, h_name) ∈ ((NormL0(λ), "l0"), (NormL1(λ), "l1"), (IndBallL0(10 * compound), "B0"))
+    for solver_sym ∈ (:R2, :TR)
+      solver_sym == :TR && mod_name == "exact" && continue
+      solver_name = string(solver_sym)
+      solver = eval(solver_sym)
+      @testset "bpdn-$(mod_name)-$(solver_name)-$(h_name)" begin
+        x0 = zeros(bpdn.meta.nvar)
+        p  = randperm(bpdn.meta.nvar)[1:nz]
+        x0[p[1:nz]] = sign.(randn(nz))  # initial guess with nz nonzeros (necessary for h = B0)
+        args = solver_sym == :R2 ? () : (NormLinf(1.0),)
+        x, Fhist, Hhist, Comp_pg, ξ = solver(mod(bpdn), h, args..., options, x0 = x0)
+        @test typeof(x) == typeof(bpdn.meta.x0)
+        @test length(x) == bpdn.meta.nvar
+        @test typeof(Fhist) == typeof(x)
+        @test typeof(Hhist) == typeof(x)
+        @test typeof(Comp_pg) == Matrix{Int}
+        @test typeof(ξ) == eltype(x)
+        @test length(Fhist) == length(Hhist)
+        @test length(Fhist) == size(Comp_pg, 2)
+        @test obj(bpdn, x) == Fhist[end]
+        @test h(x) == Hhist[end]
+        @test sqrt(ξ) < options.ϵ
+      end
+    end
+  end
 end
+
+# TR with h = L1 and χ = L2 is a special case
+for (mod, mod_name) ∈ ((LSR1Model, "lsr1"), (LBFGSModel, "lbfgs"))
+  for (h, h_name) ∈ ((NormL1(λ), "l1"),)
+    @testset "bpdn-$(mod_name)-TR-$(h_name)" begin
+      x0 = zeros(bpdn.meta.nvar)
+      p  = randperm(bpdn.meta.nvar)[1:nz]
+      x0[p[1:nz]] = sign.(randn(nz))  # initial guess with nz nonzeros (necessary for h = B0)
+      x, Fhist, Hhist, Comp_pg, ξ = TR(mod(bpdn), h, NormL2(1.0), options, x0 = x0)
+      @test typeof(x) == typeof(bpdn.meta.x0)
+      @test length(x) == bpdn.meta.nvar
+      @test typeof(Fhist) == typeof(x)
+      @test typeof(Hhist) == typeof(x)
+      @test typeof(Comp_pg) == Matrix{Int}
+      @test typeof(ξ) == eltype(x)
+      @test length(Fhist) == length(Hhist)
+      @test length(Fhist) == size(Comp_pg, 2)
+      @test obj(bpdn, x) == Fhist[end]
+      @test h(x) == Hhist[end]
+      @test sqrt(ξ) < options.ϵ
+    end
+  end
+end
+
