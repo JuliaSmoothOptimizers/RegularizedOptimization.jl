5d50b57e59fbda36e1f891034260a41eaa337396
Dominique Orban <dominique.orban@gmail.com>
2022-09-09 16:54:50 -0400

    attempt to decrease number of Arpack failures

--- src/utils.jl
+++ src/utils.jl
@@ -1,11 +1,18 @@
 # use Arpack to obtain largest eigenvalue in magnitude with a minimum of robustness
 function LinearAlgebra.opnorm(B; kwargs...)
-  m, n = size(B)
-  opnorm_fcn = m == n ? opnorm_eig : opnorm_svd
-  return opnorm_fcn(B; kwargs...)
+  hermitian =
+    (typeof(B) <: AbstractQuasiNewtonOperator && ishermitian(B)) ||
+    (typeof(B) <: Symmetric && eltype(B) <: Real) ||
+    (typeof(B) <: Hermitian && eltype(B) <: Complex)
+  opnorm_fcn = hermitian ? opnorm_eig : opnorm_svd
+  nrm, success = opnorm_fcn(B; kwargs...)
+  if !success
+    nrm, _ = normest(B)
+  end
+  return nrm
 end
 
-function opnorm_eig(B; max_attempts::Int = 3)
+function opnorm_eig(B; max_attempts::Int = 10)
   have_eig = false
   attempt = 0
   λ = zero(eltype(B))
@@ -28,7 +35,7 @@ function opnorm_eig(B; max_attempts::Int = 3)
   return λ, success
 end
 
-function opnorm_svd(J; max_attempts::Int = 3)
+function opnorm_svd(J; max_attempts::Int = 10)
   have_svd = false
   attempt = 0
   σ = zero(eltype(J))
