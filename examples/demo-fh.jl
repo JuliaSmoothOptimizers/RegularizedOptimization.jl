bf15b52018dec025354b9a837cbd9fff7fd99caf
Dominique Orban <dominique.orban@gmail.com>
2022-09-16 16:08:26 -0400

    run demos with CI

--- examples/demo-fh.jl
+++ examples/demo-fh.jl
@@ -8,17 +8,20 @@ function demo_fh()
   model = ADNLPModel(misfit, ones(5))
   h = NormL0(1.0)
   χ = NormLinf(1.0)
-  options = ROSolverOptions(; verbose = 10, ϵ = 1e-6, β = 1e16, ν = 1.0e+2)
+  # relax tolerances to speed up demo
+  options = ROSolverOptions(; verbose = 10, ϵa = 1e-3, ϵr = 1e-3, β = 1e16, ν = 1.0e+2)
 
   lbfgs_model = LBFGSModel(model)
   TR_out = TR(lbfgs_model, h, χ, options)
   plot_fh(TR_out, simulate(TR_out.solution), data, "tr-r2")
 
   nls_model = ADNLSModel(resid, ones(5), 202)
+  options.σmin = 1e-6
   LMTR_out = LMTR(nls_model, h, χ, options)
   plot_fh(LMTR_out, simulate(LMTR_out.solution), data, "lmtr-r2")
 
   reset!(nls_model)
+  options.σmin = 1e+3
   LM_out = LM(nls_model, h, options)
   plot_fh(LM_out, simulate(LM_out.solution), data, "lm-r2")
 end
