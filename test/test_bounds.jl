2ad6ff25cf69027f782269ad67851f8fc7e9f04a
geoffroyleconte <47035783+geoffroyleconte@users.noreply.github.com>
2023-02-14 18:08:03 -0500

    LMTR with bounds (#87)

--- test/test_bounds.jl
+++ test/test_bounds.jl
@@ -25,3 +25,27 @@ for (mod, mod_name) ∈ ((x -> x, "exact"), (LSR1Model, "lsr1"), (LBFGSModel, "l
     end
   end
 end
+
+for (h, h_name) ∈ ((NormL0(λ), "l0"), (NormL1(λ), "l1"))
+  for solver_sym ∈ (:LMTR,)
+    solver_name = string(solver_sym)
+    solver = eval(solver_sym)
+    @testset "bpdn-with-bounds-ls-$(solver_name)-$(h_name)" begin
+      x0 = zeros(bpdn_nls2.meta.nvar)
+      args = solver_sym == :LM ? () : (NormLinf(1.0),)
+      @test has_bounds(bpdn_nls2)
+      out = solver(bpdn_nls2, h, args..., options, x0 = x0)
+      @test typeof(out.solution) == typeof(bpdn_nls2.meta.x0)
+      @test length(out.solution) == bpdn_nls2.meta.nvar
+      @test typeof(out.solver_specific[:Fhist]) == typeof(out.solution)
+      @test typeof(out.solver_specific[:Hhist]) == typeof(out.solution)
+      @test typeof(out.solver_specific[:SubsolverCounter]) == Array{Int, 1}
+      @test typeof(out.dual_feas) == eltype(out.solution)
+      @test length(out.solver_specific[:Fhist]) == length(out.solver_specific[:Hhist])
+      @test length(out.solver_specific[:Fhist]) == length(out.solver_specific[:SubsolverCounter])
+      @test obj(bpdn_nls2, out.solution) == out.solver_specific[:Fhist][end]
+      @test h(out.solution) == out.solver_specific[:Hhist][end]
+      @test out.status == :first_order
+    end
+  end
+end
