7f65711c6581daf502cd9b9778390fc104393192
geoffroyleconte <47035783+geoffroyleconte@users.noreply.github.com>
2023-02-14 18:08:03 -0500

    LMTR with bounds (#87)

--- examples/demo-bpdn-constr.jl
+++ examples/demo-bpdn-constr.jl
@@ -8,7 +8,7 @@ include("plot-utils-bpdn.jl")
 
 Random.seed!(1234)
 
-function demo_solver(f, sol, h, χ, suffix = "l0-linf")
+function demo_solver(f, nls, sol, h, χ, suffix = "l0-linf")
   options = ROSolverOptions(ν = 1.0, β = 1e16, ϵa = 1e-6, ϵr = 1e-6, verbose = 10)
 
   @info " using TR to solve with" h χ
@@ -22,15 +22,21 @@ function demo_solver(f, sol, h, χ, suffix = "l0-linf")
   R2_out = R2(f, h, options, x0 = f.meta.x0)
   @info "R2 relative error" norm(R2_out.solution - sol) / norm(sol)
   plot_bpdn(R2_out, sol, "constr-r2-$(suffix)")
+  
+  @info " using LMTR to solve with" h χ
+  reset!(nls)
+  LMTR_out = LMTR(nls, h, χ, options, x0 = f.meta.x0)
+  @info "LMTR relative error" norm(LMTR_out.solution - sol) / norm(sol)
+  plot_bpdn(LMTR_out, sol, "constr-lmtr-r2-$(suffix)")
 end
 
 function demo_bpdn_constr(compound = 1)
   model, nls_model, sol = bpdn_model(compound, bounds = true)
   f = LSR1Model(model)
   λ = norm(grad(model, zeros(model.meta.nvar)), Inf) / 10
-  demo_solver(f, sol, NormL0(λ), NormLinf(1.0))
+  demo_solver(f, nls_model, sol, NormL0(λ), NormLinf(1.0))
   λ = norm(grad(model, zeros(model.meta.nvar)), Inf) / 3
-  demo_solver(f, sol, NormL1(λ), NormLinf(1.0), "l1-linf")
+  demo_solver(f, nls_model, sol, NormL1(λ), NormLinf(1.0), "l1-linf")
 end
 
 demo_bpdn_constr()
