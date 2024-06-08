9e65f8279b4283f2c5e84bbe174dcbcb810181cb
Maxence Gollier <mgollier45@gmail.com>
2024-06-08 14:42:35 +0200

    Change R2_alg with SolverCore.solve!

--- src/R2_alg.jl
+++ src/R2_alg.jl
@@ -1,9 +1,10 @@
-export R2
+export R2,R2Solver,solve!
 
-mutable struct R2Solver{R, S <: AbstractVector{R}} <: AbstractOptimizationSolver
+mutable struct R2Solver{R <: Real,G <: Union{ShiftedProximableFunction, Nothing}, S <: AbstractVector{R}} <: AbstractOptimizationSolver
   xk::S
   ∇fk::S
   mν∇fk::S
+  ψ::G
   xkn::S
   s::S
   has_bnds::Bool
@@ -20,7 +21,8 @@ function R2Solver(
   x0::S,
   options::ROSolverOptions,
   l_bound::S,
-  u_bound::S,
+  u_bound::S;
+  ψ = nothing
 ) where {R <: Real, S <: AbstractVector{R}}
   maxIter = options.maxIter
   xk = similar(x0)
@@ -43,6 +45,7 @@ function R2Solver(
     xk,
     ∇fk,
     mν∇fk,
+    ψ,
     xkn,
     s,
     has_bnds,
@@ -55,6 +58,53 @@ function R2Solver(
     Complex_hist,
   )
 end
+
+function R2Solver(
+  reg_nlp::AbstractRegularizedNLPModel{T,V};
+  max_iter::Int = 10000,
+  kwargs...
+) where {T,V}
+  kwargs_dict = Dict(kwargs...)
+  x0 = pop!(kwargs_dict, :x0, reg_nlp.model.meta.x0)
+  l_bound = reg_nlp.model.meta.lvar
+  u_bound = reg_nlp.model.meta.uvar
+
+  xk = similar(x0)
+  ∇fk = similar(x0)
+  mν∇fk = similar(x0)
+  xkn = similar(x0)
+  s = zero(x0)
+  has_bnds = any(l_bound .!= T(-Inf)) || any(u_bound .!= T(Inf))
+  if has_bnds
+    l_bound_m_x = similar(xk)
+    u_bound_m_x = similar(xk)
+  else
+    l_bound_m_x = similar(xk, 0)
+    u_bound_m_x = similar(xk, 0)
+  end
+  Fobj_hist = zeros(T, max_iter)
+  Hobj_hist = zeros(T, max_iter)
+  Complex_hist = zeros(Int, max_iter)
+  
+  ψ = has_bnds ? shifted(reg_nlp.h, x0, l_bound_m_x, u_bound_m_x, reg_nlp.selected) : shifted(reg_nlp.h, x0)
+  return R2Solver(
+    xk,
+    ∇fk,
+    mν∇fk,
+    ψ,
+    xkn,
+    s,
+    has_bnds,
+    l_bound,
+    u_bound,
+    l_bound_m_x,
+    u_bound_m_x,
+    Fobj_hist,
+    Hobj_hist,
+    Complex_hist,
+  )
+end
+
 
 """
     R2(reg_nlp, options)
@@ -102,43 +152,33 @@ In the third form, instead of `nlp`, the user may pass in
 * `Complex_hist`: an array with the history of number of inner iterations.
 """
 
-function R2(nlp::AbstractNLPModel, h, args...; kwargs...)
+function R2(
+  nlp::AbstractNLPModel{R, V}, 
+  h, 
+  options::ROSolverOptions{R};
+  kwargs...) where{ R <: Real, V }
   kwargs_dict = Dict(kwargs...)
   selected = pop!(kwargs_dict, :selected, 1:nlp.meta.nvar) 
+  x0 = pop!(kwargs_dict, :x0, nlp.meta.x0)
   reg_nlp = RegularizedNLPModel(nlp, h, selected)
-  return R2(reg_nlp, args...; kwargs...)
-end
-
-function R2(reg_nlp::AbstractRegularizedNLPModel, args...; kwargs...)
-  kwargs_dict = Dict(kwargs...)
-  x0 = pop!(kwargs_dict, :x0, reg_nlp.model.meta.x0)
-  xk, k, outdict = R2(
-    x -> obj(reg_nlp.model, x),
-    (g, x) -> grad!(reg_nlp.model, x, g),
-    reg_nlp.h,
-    args...,
-    x0,
-    reg_nlp.model.meta.lvar,
-    reg_nlp.model.meta.uvar;
-    selected = reg_nlp.selected,
-    kwargs_dict...,
+  return R2(
+    reg_nlp,
+    x = x0,
+    a_tol = options.ϵa,
+    r_tol = options.ϵr,
+    neg_tol = options.neg_tol,
+    verbose = options.verbose,
+    max_iter = options.maxIter,
+    max_time = options.maxTime,
+    σmin = options.σmin,
+    η1 = options.η1,
+    η2 = options.η2,
+    ν = options.ν,
+    γ = options.γ,
   )
-  ξ = outdict[:ξ]
-  stats = GenericExecutionStats(reg_nlp.model)
-  set_status!(stats, outdict[:status])
-  set_solution!(stats, xk)
-  set_objective!(stats, outdict[:fk] + outdict[:hk])
-  set_residuals!(stats, zero(eltype(xk)), ξ)
-  set_iter!(stats, k)
-  set_time!(stats, outdict[:elapsed_time])
-  set_solver_specific!(stats, :Fhist, outdict[:Fhist])
-  set_solver_specific!(stats, :Hhist, outdict[:Hhist])
-  set_solver_specific!(stats, :NonSmooth, outdict[:NonSmooth])
-  set_solver_specific!(stats, :SubsolverCounter, outdict[:Chist])
   return stats
 end
 
-# method without bounds
 function R2(
   f::F,
   ∇f!::G,
@@ -148,23 +188,35 @@ function R2(
   selected::AbstractVector{<:Integer} = 1:length(x0),
   kwargs...,
 ) where {F <: Function, G <: Function, H, R <: Real}
-  start_time = time()
-  elapsed_time = 0.0
-  solver = R2Solver(x0, options, similar(x0, 0), similar(x0, 0))
-  k, status, fk, hk, ξ = R2!(solver, f, ∇f!, h, options, x0; selected = selected)
-  elapsed_time = time() - start_time
-  outdict = Dict(
-    :Fhist => solver.Fobj_hist[1:k],
-    :Hhist => solver.Hobj_hist[1:k],
-    :Chist => solver.Complex_hist[1:k],
+  nlp = FirstOrderModel(f,∇f!,x0)
+  reg_nlp = RegularizedNLPModel(nlp,h,selected) 
+  stats = R2(
+  reg_nlp,
+  x=x0,
+  a_tol = options.ϵa,
+  r_tol = options.ϵr,
+  neg_tol = options.neg_tol,
+  verbose = options.verbose,
+  max_iter = options.maxIter,
+  max_time = options.maxTime,
+  σmin = options.σmin,
+  η1 = options.η1,
+  η2 = options.η2,
+  ν = options.ν,
+  γ = options.γ,
+)
+outdict = Dict(
+    :Fhist => stats.solver_specific[:Fhist],
+    :Hhist => stats.solver_specific[:Hhist],
+    :Chist => stats.solver_specific[:SubsolverCounter],
     :NonSmooth => h,
-    :status => status,
-    :fk => fk,
-    :hk => hk,
-    :ξ => ξ,
-    :elapsed_time => elapsed_time,
+    :status => stats.status,
+    :fk => stats.solver_specific[:smooth_obj],
+    :hk => stats.solver_specific[:nonsmooth_obj],
+    :ξ => stats.solver_specific[:xi],
+    :elapsed_time => stats.elapsed_time,
   )
-  return solver.xk, k, outdict
+return stats.solution,stats.iter,outdict
 end
 
 function R2(
@@ -178,53 +230,93 @@ function R2(
   selected::AbstractVector{<:Integer} = 1:length(x0),
   kwargs...,
 ) where {F <: Function, G <: Function, H, R <: Real}
-  start_time = time()
-  elapsed_time = 0.0
-  solver = R2Solver(x0, options, l_bound, u_bound)
-  k, status, fk, hk, ξ = R2!(solver, f, ∇f!, h, options, x0; selected = selected)
-  elapsed_time = time() - start_time
-  outdict = Dict(
-    :Fhist => solver.Fobj_hist[1:k],
-    :Hhist => solver.Hobj_hist[1:k],
-    :Chist => solver.Complex_hist[1:k],
-    :NonSmooth => h,
-    :status => status,
-    :fk => fk,
-    :hk => hk,
-    :ξ => ξ,
-    :elapsed_time => elapsed_time,
+  nlp = FirstOrderModel(f,∇f!,x0,lcon = l_bound, ucon = u_bound)
+  reg_nlp = RegularizedNLPModel(nlp,h,selected) 
+  stats = R2(
+    reg_nlp,
+    x=x0,
+    a_tol = options.ϵa,
+    r_tol = options.ϵr,
+    neg_tol = options.neg_tol,
+    verbose = options.verbose,
+    max_iter = options.maxIter,
+    max_time = options.maxTime,
+    σmin = options.σmin,
+    η1 = options.η1,
+    η2 = options.η2,
+    ν = options.ν,
+    γ = options.γ,
   )
-  return solver.xk, k, outdict
+  outdict = Dict(
+    :Fhist => stats.solver_specific[:Fhist],
+    :Hhist => stats.solver_specific[:Hhist],
+    :Chist => stats.solver_specific[:SubsolverCounter],
+    :NonSmooth => h,
+    :status => stats.status,
+    :fk => stats.solver_specific[:smooth_obj],
+    :hk => stats.solver_specific[:nonsmooth_obj],
+    :ξ => stats.solver_specific[:xi],
+    :elapsed_time => stats.elapsed_time,
+  )
+  return stats.solution,stats.iter,outdict
 end
 
-function R2!(
-  solver::R2Solver{R},
-  f::F,
-  ∇f!::G,
-  h::H,
-  options::ROSolverOptions{R},
-  x0::AbstractVector{R};
-  selected::AbstractVector{<:Integer} = 1:length(x0),
-) where {F <: Function, G <: Function, H, R <: Real}
-  start_time = time()
-  elapsed_time = 0.0
-  ϵ = options.ϵa
-  ϵr = options.ϵr
-  neg_tol = options.neg_tol
-  verbose = options.verbose
-  maxIter = options.maxIter
-  maxTime = options.maxTime
-  σmin = options.σmin
-  η1 = options.η1
-  η2 = options.η2
-  ν = options.ν
-  γ = options.γ
 
-  # retrieve workspace
-  xk = solver.xk
-  xk .= x0
+function R2(reg_nlp::AbstractRegularizedNLPModel; kwargs...)
+  kwargs_dict = Dict(kwargs...)
+  x0 = pop!(kwargs_dict, :x, reg_nlp.model.meta.x0)
+  solver = R2Solver(reg_nlp,x0=x0)
+  stats = GenericExecutionStats(reg_nlp.model)
+  cb = (nlp, solver, stats) -> begin
+    solver.Fobj_hist[stats.iter+1] = stats.solver_specific[:smooth_obj]
+    solver.Hobj_hist[stats.iter+1] = stats.solver_specific[:nonsmooth_obj]
+    solver.Complex_hist[stats.iter+1] += 1
+  end
+  solve!(
+    solver,
+    reg_nlp,
+    stats;
+    x = x0,
+    callback = cb,
+    kwargs...
+  )
+  set_solver_specific!(stats, :Fhist, solver.Fobj_hist[1:stats.iter+1])
+  set_solver_specific!(stats, :Hhist, solver.Hobj_hist[1:stats.iter+1])
+  set_solver_specific!(stats, :SubsolverCounter, solver.Complex_hist[1:stats.iter+1])
+  return stats
+end
+
+function SolverCore.solve!(
+  solver::R2Solver{T}, 
+  reg_nlp::AbstractRegularizedNLPModel{T, V}, 
+  stats::GenericExecutionStats{T, V};
+  callback = (args...) -> nothing,
+  x::V = reg_nlp.model.meta.x0,
+  a_tol::T = √eps(T),
+  r_tol::T = √eps(T),
+  neg_tol::T = eps(T)^(1 / 4),
+  verbose::Int = 0,
+  max_iter::Int = 10000,
+  max_time::Float64 = 30.0,
+  max_eval::Int = -1,
+  σmin::T = eps(T),
+  η1::T = √√eps(T),
+  η2::T = T(0.9),
+  ν::T = eps(T)^(1 / 5),
+  γ::T = T(3),
+  ) where {T, V}
+
+  reset!(stats)
+
+  # Retrieve workspace
+  selected = reg_nlp.selected
+  h = reg_nlp.h
+  nlp = reg_nlp.model
+
+  xk = solver.xk .= x
   ∇fk = solver.∇fk
   mν∇fk = solver.mν∇fk
+  ψ = solver.ψ
   xkn = solver.xkn
   s = solver.s
   has_bnds = solver.has_bnds
@@ -234,103 +326,101 @@ function R2!(
     l_bound_m_x = solver.l_bound_m_x
     u_bound_m_x = solver.u_bound_m_x
   end
-  Fobj_hist = solver.Fobj_hist
-  Hobj_hist = solver.Hobj_hist
-  Complex_hist = solver.Complex_hist
-
-  if verbose == 0
-    ptf = Inf
-  elseif verbose == 1
-    ptf = round(maxIter / 10)
-  elseif verbose == 2
-    ptf = round(maxIter / 100)
-  else
-    ptf = 1
-  end
 
   # initialize parameters
+  improper = false
   hk = @views h(xk[selected])
   if hk == Inf
     verbose > 0 && @info "R2: finding initial guess where nonsmooth term is finite"
-    prox!(xk, h, x0, one(eltype(x0)))
+    prox!(xk, h, xk, one(eltype(x0)))
     hk = @views h(xk[selected])
     hk < Inf || error("prox computation must be erroneous")
     verbose > 0 && @debug "R2: found point where h has value" hk
   end
-  hk == -Inf && error("nonsmooth term is not proper")
+   improper = (hk == -Inf)
 
   if has_bnds
     @. l_bound_m_x = l_bound - xk
     @. u_bound_m_x = u_bound - xk
-    ψ = shifted(h, xk, l_bound_m_x, u_bound_m_x, selected)
-  else
-    ψ = shifted(h, xk)
   end
 
   if verbose > 0
-    #! format: off
-    @info @sprintf "%6s %8s %8s %7s %8s %7s %7s %7s %1s" "iter" "f(x)" "h(x)" "√(ξ/ν)" "ρ" "σ" "‖x‖" "‖s‖" ""
-    #! format: off
+    @info log_header(
+      [:iter, :fx, :hx, :xi, :ρ, :σ, :normx, :norms, :arrow],
+      [Int, Float64, Float64, Float64, Float64, Float64, Float64, Float64, Char],
+      hdr_override = Dict{Symbol,String}(   # TODO: Add this as constant dict elsewhere
+        :iter => "iter",
+        :fx => "f(x)",
+        :hx => "h(x)",
+        :xi => "√(ξ/ν)",
+        :ρ => "ρ",
+        :σ => "σ",
+        :normx => "‖x‖",
+        :norms => "‖s‖",
+        :arrow => " "
+      ),
+      colsep = 1,
+    )
   end
 
-  local ξ::R
-  k = 0
+  local ξ::T
   σk = max(1 / ν, σmin)
   ν = 1 / σk
-  sqrt_ξ_νInv = one(R)
+  sqrt_ξ_νInv = one(T)
 
-  fk = f(xk)
-  ∇f!(∇fk, xk)
+  fk = obj(nlp, xk)
+  grad!(nlp, xk, ∇fk)
   @. mν∇fk = -ν * ∇fk
 
-  optimal = false
-  tired = maxIter > 0 && k ≥ maxIter || elapsed_time > maxTime
+  set_iter!(stats, 0)
+  start_time = time()
+  set_time!(stats, 0.0)
+  set_objective!(stats,fk + hk)
+  set_solver_specific!(stats,:smooth_obj,fk)
+  set_solver_specific!(stats,:nonsmooth_obj, hk)
 
-  while !(optimal || tired)
-    k = k + 1
-    elapsed_time = time() - start_time
-    Fobj_hist[k] = fk
-    Hobj_hist[k] = hk
+  φk(d) = dot(∇fk, d)   
+  mk(d)::T = φk(d) + ψ(d)::T
 
-    # define model
-    φk(d) = dot(∇fk, d)
-    mk(d)::R = φk(d) + ψ(d)::R
+  prox!(s, ψ, mν∇fk, ν)
+  mks = mk(s)
 
-    prox!(s, ψ, mν∇fk, ν)
-    Complex_hist[k] += 1
-    mks = mk(s)
-    ξ = hk - mks + max(1, abs(hk)) * 10 * eps()
-    sqrt_ξ_νInv = ξ ≥ 0 ? sqrt(ξ / ν) : sqrt(-ξ / ν)
+  ξ = hk - mks + max(1, abs(hk)) * 10 * eps()
+  ξ > 0 || error("R2: prox-gradient step should produce a decrease but ξ = $(ξ)")
+  sqrt_ξ_νInv = ξ ≥ 0 ? sqrt(ξ / ν) : sqrt(-ξ / ν)
+  a_tol += r_tol * sqrt_ξ_νInv # make stopping test absolute and relative
 
-    if ξ ≥ 0 && k == 1
-      ϵ += ϵr * sqrt_ξ_νInv # make stopping test absolute and relative
-    end
+  solved = (ξ < 0 && sqrt_ξ_νInv ≤ neg_tol) || (ξ ≥ 0 && sqrt_ξ_νInv ≤ a_tol)
+
+  set_solver_specific!(stats,:xi,sqrt_ξ_νInv)
+  set_status!(
+    stats,
+    get_status(
+      reg_nlp,
+      elapsed_time = stats.elapsed_time,
+      iter = stats.iter,
+      optimal = solved,
+      improper = improper,
+      max_eval = max_eval,
+      max_time = max_time,
+      max_iter = max_iter
+    ),
+  )
+
+  callback(nlp, solver, stats)
+
+  done = stats.status != :unknown
+
+  while !done
     
-    if (ξ < 0 && sqrt_ξ_νInv ≤ neg_tol) || (ξ ≥ 0 && sqrt_ξ_νInv ≤ ϵ)
-      optimal = true
-      continue
-    end
-
-    ξ > 0 || error("R2: prox-gradient step should produce a decrease but ξ = $(ξ)")
+    # Update xk, sigma_k
     xkn .= xk .+ s
-    fkn = f(xkn)
+    fkn = obj(nlp, xkn)
     hkn = @views h(xkn[selected])
-    hkn == -Inf && error("nonsmooth term is not proper")
+    improper = (hkn == -Inf)
 
     Δobj = (fk + hk) - (fkn + hkn) + max(1, abs(fk + hk)) * 10 * eps()
     ρk = Δobj / ξ
-
-    if (verbose > 0) && (k % ptf == 0)
-      #! format: off
-      σ_stat = (η2 ≤ ρk < Inf) ? "↘" : (ρk < η1 ? "↗" : "=")
-      @info @sprintf "%6d %8.1e %8.1e %7.1e %8.1e %7.1e %7.1e %7.1e %1s" k fk hk sqrt_ξ_νInv ρk σk norm(xk) norm(s) σ_stat
-
-      #! format: on
-    end
-
-    if η2 ≤ ρk < Inf
-      σk = max(σk / γ, σmin)
-    end
 
     if η1 ≤ ρk < Inf
       xk .= xkn
@@ -341,41 +431,86 @@ function R2!(
       end
       fk = fkn
       hk = hkn
-      ∇f!(∇fk, xk)
+      grad!(nlp, xk, ∇fk)
       shift!(ψ, xk)
     end
 
+    if η2 ≤ ρk < Inf
+      σk = max(σk / γ, σmin)
+    end
     if ρk < η1 || ρk == Inf
       σk = σk * γ
     end
 
     ν = 1 / σk
-    tired = maxIter > 0 && k ≥ maxIter
-    if !tired
-      @. mν∇fk = -ν * ∇fk
-    end
+    @. mν∇fk = -ν * ∇fk
+  
+    set_objective!(stats, fk + hk)
+    set_solver_specific!(stats,:smooth_obj,fk)
+    set_solver_specific!(stats,:nonsmooth_obj, hk)
+    set_iter!(stats, stats.iter + 1)
+    set_time!(stats, time() - start_time)
+
+    prox!(s, ψ, mν∇fk, ν)
+    mks = mk(s)
+
+    ξ = hk - mks + max(1, abs(hk)) * 10 * eps()
+    sqrt_ξ_νInv = ξ ≥ 0 ? sqrt(ξ / ν) : sqrt(-ξ / ν)
+    solved = (ξ < 0 && sqrt_ξ_νInv ≤ neg_tol) || (ξ ≥ 0 && sqrt_ξ_νInv ≤ a_tol)
+  
+    verbose > 0 && 
+      stats.iter % verbose == 0 &&
+        @info log_row(Any[stats.iter, fk, hk, sqrt_ξ_νInv, ρk, σk, norm(xk), norm(s), (η2 ≤ ρk < Inf) ? "↘" : (ρk < η1 ? "↗" : "=")], colsep = 1)
+
+    set_solver_specific!(stats,:xi,sqrt_ξ_νInv)
+    set_status!(
+      stats,
+      get_status(
+        reg_nlp,
+        elapsed_time = stats.elapsed_time,
+        iter = stats.iter,
+        optimal = solved,
+        improper = improper,
+        max_eval = max_eval,
+        max_time = max_time,
+        max_iter = max_iter
+      ),
+    )
+
+    callback(nlp, solver, stats)
+
+    done = stats.status != :unknown
   end
 
-  if verbose > 0
-    if k == 1
-      @info @sprintf "%6d %8.1e %8.1e" k fk hk
-    elseif optimal
-      #! format: off
-      @info @sprintf "%6d %8.1e %8.1e %7.1e %8s %7.1e %7.1e %7.1e" k fk hk sqrt(ξ/ν) "" σk norm(xk) norm(s)
-      #! format: on
-      @info "R2: terminating with √(ξ/ν) = $(sqrt_ξ_νInv)"
-    end
-  end
+  verbose > 0 &&
+    stats.status == :first_order &&
+        @info "R2: terminating with √(ξ/ν) = $(sqrt_ξ_νInv)"
 
-  status = if optimal
+  set_solution!(stats,xk)
+  return stats
+end
+
+function get_status(
+  reg_nlp;
+  elapsed_time = 0.0,
+  iter = 0,
+  optimal = false,
+  improper = false,
+  max_eval = Inf,
+  max_time = Inf,
+  max_iter = Inf,
+)
+  if optimal
     :first_order
-  elseif elapsed_time > maxTime
-    :max_time
-  elseif tired
+  elseif improper
+    :improper
+  elseif iter > max_iter
     :max_iter
+  elseif elapsed_time > max_time
+    :max_time
+  elseif neval_obj(reg_nlp.model) > max_eval && max_eval != -1
+    :max_eval
   else
-    :exception
+    :unknown
   end
-
-  return k, status, fk, hk, sqrt_ξ_νInv
 end
