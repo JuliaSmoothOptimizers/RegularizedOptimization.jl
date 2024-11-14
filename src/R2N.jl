e0a840b023150ab79131d8cd10a89f04f7c9e47a
MohamedLaghdafHABIBOULLAH <mohamed-laghdaf-2.habiboullah@polymtl.ca>
2024-11-14 14:28:06 -0500

    Update doc

--- src/R2N.jl
+++ src/R2N.jl
@@ -7,7 +7,7 @@ A regularized quasi-Newton method for the problem
 
     min f(x) + h(x)
 
-where f: ℝⁿ → ℝ is C¹ and h: ℝⁿ → ℝ is lower semi-continuous and proper.
+where f: ℝⁿ → ℝ is C¹ and h: ℝⁿ → ℝ is lower semi-continuous, proper and prox-bounded.
 
 About each iterate xₖ, a step sₖ is computed as an approximate solution of
 
