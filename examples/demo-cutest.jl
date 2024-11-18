3df8f2ad11b7013fe9f5043184be6e6157880e4c
Alberto De Marchi <aldmarchi@gmail.com>
2024-11-18 10:15:20 +0100

    add ALSolver and solve!, uniform kwargs

--- examples/demo-cutest.jl
+++ examples/demo-cutest.jl
@@ -12,4 +12,21 @@ h = NormL1(1.0)
 stats = AL(nlp, h, atol = 1e-6, verbose = 1)
 print(stats)
 
+using RegularizedProblems
+
+regnlp = RegularizedNLPModel(nlp, h)
+stats = AL(regnlp, atol = 1e-6, verbose = 1)
+print(stats)
+
+solver = ALSolver(regnlp)
+stats = solve!(solver, regnlp, atol = 1e-6, verbose = 1)
+print(stats)
+
+using SolverCore
+
+stats = GenericExecutionStats(nlp)
+solver = ALSolver(regnlp)
+stats = solve!(solver, regnlp, stats, atol = 1e-6, verbose = 1)
+print(stats)
+
 finalize(nlp)
