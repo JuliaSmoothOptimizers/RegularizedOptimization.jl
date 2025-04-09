f063735903d7c646e2625fb65f32205c5e813669
Maxence Gollier <134112149+MaxenceGollier@users.noreply.github.com>
2025-04-09 11:11:31 -0400

    add step entry in RegularizedExecutionStats (#179)
    add :sigma and :radius solver specific entries to LM,LMTR,R2DH,R2N,TR,TRDH

--- src/LM_alg.jl
+++ src/LM_alg.jl
@@ -290,6 +290,7 @@ function LM(
   set_residuals!(stats, zero(eltype(xk)), ξ1 ≥ 0 ? sqrt_ξ1_νInv : ξ1)
   set_iter!(stats, k)
   set_time!(stats, elapsed_time)
+  set_solver_specific!(stats, :sigma, σk)
   set_solver_specific!(stats, :Fhist, Fobj_hist[1:k])
   set_solver_specific!(stats, :Hhist, Hobj_hist[1:k])
   set_solver_specific!(stats, :NonSmooth, h)
