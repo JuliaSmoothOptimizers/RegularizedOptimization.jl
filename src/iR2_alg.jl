4592de535ddda9c01adbf19f98dcb23e2b8ab1bc
nathanemac <91251698+nathanemac@users.noreply.github.com>
2025-04-09 17:23:25 -0400

    small changes. trying to make ProxTV non allocating

--- src/iR2_alg.jl
+++ src/iR2_alg.jl
@@ -572,7 +572,7 @@ function SolverCore.solve!(
     @info "iR2: terminating with √(ξ/ν) = $(sqrt_ξ_νInv)"
   end
 
-  set_solver_specific!(stats, :ItersProx, ψ.h.context.prox_stats[3])
+  # set_solver_specific!(stats, :ItersProx, ψ.h.context.prox_stats[3])
   set_solution!(stats, xk)
   return stats
 end
