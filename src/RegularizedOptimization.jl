b7646cec618266fee3793ff063af58cc9df72b6e
Maxence Gollier <mgollier45@gmail.com>
2025-09-08 10:22:31 -0400

    add ManualNLPModels dep

--- src/RegularizedOptimization.jl
+++ src/RegularizedOptimization.jl
@@ -8,7 +8,7 @@ using Arpack, ProximalOperators
 
 # dependencies from us
 using LinearOperators,
-  NLPModels, NLPModelsModifiers, RegularizedProblems, ShiftedProximalOperators, SolverCore
+  ManualNLPModels, NLPModels, NLPModelsModifiers, RegularizedProblems, ShiftedProximalOperators, SolverCore
 using Percival: AugLagModel, update_y!, update_μ!
 
 const callback_docstring = "
