6ea9bea04aee6c73b78b3d008369868fe7f87679
Maxence Gollier <mgollier45@gmail.com>
2025-09-26 15:09:35 -0400

    use deps in tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,15 @@
-using RegularizedOptimization
-using Test
+using LinearAlgebra: length
+using LinearAlgebra, Random, Test
+using ProximalOperators
+using ADNLPModels,
+  OptimizationProblems,
+  OptimizationProblems.ADNLPProblems,
+  NLPModels,
+  NLPModelsModifiers,
+  RegularizedProblems,
+  RegularizedOptimization,
+  SolverCore
 
-#=
-Don't add your tests to runtests.jl. Instead, create files named
-
-    test-title-for-my-test.jl
-
-The file will be automatically included inside a `@testset` with title "Title For My Test".
-=#
 for (root, dirs, files) in walkdir(@__DIR__)
   for file in files
     if isnothing(match(r"^test-.*\.jl$", file))
