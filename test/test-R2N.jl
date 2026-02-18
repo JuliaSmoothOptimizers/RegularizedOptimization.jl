a364151c696a866dfb6006c7c4c505a8dc676f83
Maxence Gollier <mgollier45@gmail.com>
2026-02-18 15:00:35 -0500

    apply suggestions

--- test/test-R2N.jl
+++ test/test-R2N.jl
@@ -9,35 +9,35 @@
     first_order_kwargs = (atol = 1e-6, rtol = 1e-6)
     test_solver(
       rosenbrock_reg_nlp,
-      "R2N",
+      R2N,
       expected_status = :first_order,
       solver_kwargs = first_order_kwargs,
     )
     solver, stats = R2NSolver(rosenbrock_reg_nlp), RegularizedExecutionStats(rosenbrock_reg_nlp)
 
     # Test max time status
-    max_time_kwargs = (x0 = [π, -π], atol = 1e-16, rtol = 1e-16, max_time = 1e-12)
+    max_time_kwargs = (x = [π, -π], atol = 1e-16, rtol = 1e-16, max_time = 1e-12)
     test_solver(
       rosenbrock_reg_nlp,
-      "R2N",
+      R2N,
       expected_status = :max_time,
       solver_kwargs = max_time_kwargs,
     )
 
     # Test max iter status
-    max_iter_kwargs = (x0 = [π, -π], atol = 1e-16, rtol = 1e-16, max_iter = 1)
+    max_iter_kwargs = (x = [π, -π], atol = 1e-16, rtol = 1e-16, max_iter = 1)
     test_solver(
       rosenbrock_reg_nlp,
-      "R2N",
+      R2N,
       expected_status = :max_iter,
       solver_kwargs = max_iter_kwargs,
     )
 
     # Test max eval status
-    max_eval_kwargs = (x0 = [π, -π], atol = 1e-16, rtol = 1e-16, max_eval = 1)
+    max_eval_kwargs = (x = [π, -π], atol = 1e-16, rtol = 1e-16, max_eval = 1)
     test_solver(
       rosenbrock_reg_nlp,
-      "R2N",
+      R2N,
       expected_status = :max_eval,
       solver_kwargs = max_eval_kwargs,
     )
@@ -50,10 +50,10 @@
         stats.status = :user
       end
     end
-    callback_kwargs = (x0 = [π, -π], atol = 1e-16, rtol = 1e-16, callback = callback)
+    callback_kwargs = (x = [π, -π], atol = 1e-16, rtol = 1e-16, callback = callback)
     test_solver(
       rosenbrock_reg_nlp,
-      "R2N",
+      R2N,
       expected_status = :user,
       solver_kwargs = callback_kwargs,
     )
@@ -62,9 +62,9 @@
   # BPDN TESTS
   # Test bpdn with L-BFGS and 1-norm
   @testset "BPDN" begin
-    bpdn_kwargs = (x0 = zeros(bpdn.meta.nvar), σk = 1.0, β = 1e16, atol = 1e-6, rtol = 1e-6)
+    bpdn_kwargs = (x = zeros(bpdn.meta.nvar), σk = 1.0, β = 1e16, atol = 1e-6, rtol = 1e-6)
     reg_nlp = RegularizedNLPModel(LBFGSModel(bpdn), NormL1(λ))
-    test_solver(reg_nlp, "R2N", expected_status = :first_order, solver_kwargs = bpdn_kwargs)
+    test_solver(reg_nlp, R2N, expected_status = :first_order, solver_kwargs = bpdn_kwargs)
     solver, stats = R2NSolver(reg_nlp), RegularizedExecutionStats(reg_nlp)
     @test @wrappedallocs(
       solve!(solver, reg_nlp, stats, σk = 1.0, β = 1e16, atol = 1e-6, rtol = 1e-6)
@@ -78,24 +78,22 @@
 
     # Test bpdn with L-SR1 and 0-norm
     reg_nlp = RegularizedNLPModel(LSR1Model(bpdn), NormL0(λ))
-    test_solver(reg_nlp, "R2N", expected_status = :first_order, solver_kwargs = bpdn_kwargs)
-    # FIXME: allocations fail with LSR1 -> a PR is awaiting on LinearOperators.jl
-    # solver, stats = R2NSolver(reg_nlp), RegularizedExecutionStats(reg_nlp)
-    # @test @wrappedallocs(
-    #   solve!(solver, reg_nlp, stats, σk = 1.0, β = 1e16, atol = 1e-6, rtol = 1e-6)
-    # ) == 0
+    test_solver(reg_nlp, R2N, expected_status = :first_order, solver_kwargs = bpdn_kwargs)
+    solver, stats = R2NSolver(reg_nlp), RegularizedExecutionStats(reg_nlp)
+    @test @wrappedallocs(
+      solve!(solver, reg_nlp, stats, σk = 1.0, β = 1e16, atol = 1e-6, rtol = 1e-6)
+    ) == 0
      
     test_solver(
       reg_nlp,
-      "R2N",
+      R2N,
       expected_status = :first_order,
       solver_kwargs = bpdn_kwargs,
       solver_constructor_kwargs = (subsolver = R2DHSolver,),
     )
-    # FIXME: allocations fail with LSR1 -> a PR is awaiting on LinearOperators.jl
-    # solver, stats = R2NSolver(reg_nlp, subsolver = R2DHSolver), RegularizedExecutionStats(reg_nlp)
-    # @test @wrappedallocs(
-    #   solve!(solver, reg_nlp, stats, σk = 1.0, β = 1e16, atol = 1e-6, rtol = 1e-6)
-    # ) == 0
+    solver, stats = R2NSolver(reg_nlp, subsolver = R2DHSolver), RegularizedExecutionStats(reg_nlp)
+    @test @wrappedallocs(
+      solve!(solver, reg_nlp, stats, σk = 1.0, β = 1e16, atol = 1e-6, rtol = 1e-6)
+    ) == 0
   end
 end
--- test/test-solver.jl
+++ test/test-solver.jl
@@ -1,22 +1,19 @@
 function test_solver(
   reg_nlp::R,
-  solver_name::String;
+  solver::F;
   expected_status = :first_order,
   solver_constructor_kwargs = (;),
   solver_kwargs = (;),
-) where {R}
+) where {R, F}
 
   # Test output with allocating calling form
-  solver_fun = getfield(RegularizedOptimization, Symbol(solver_name))
-  stats_basic = solver_fun(
-    reg_nlp.model,
-    reg_nlp.h,
-    ROSolverOptions();
+  stats_basic = solver(
+    reg_nlp;
     solver_constructor_kwargs...,
     solver_kwargs...,
   )
 
-  x0 = get(solver_kwargs, :x0, reg_nlp.model.meta.x0)
+  x0 = get(solver_kwargs, :x, reg_nlp.model.meta.x0)
   @test typeof(stats_basic.solution) == typeof(x0)
   @test length(stats_basic.solution) == reg_nlp.model.meta.nvar
   @test typeof(stats_basic.dual_feas) == eltype(stats_basic.solution)
@@ -25,13 +22,13 @@ function test_solver(
   @test stats_basic.objective <= obj(reg_nlp, x0)
 
   # Test output with optimized calling form
-  solver_constructor = getfield(RegularizedOptimization, Symbol(solver_name * "Solver"))
-  solver = solver_constructor(reg_nlp; solver_constructor_kwargs...)
+  solver_constructor = getfield(RegularizedOptimization, Symbol(string(solver) * "Solver"))
+  solver_object = solver_constructor(reg_nlp; solver_constructor_kwargs...)
   stats_optimized = RegularizedExecutionStats(reg_nlp)
 
   # Remove the x0 entry from solver_kwargs
   optimized_solver_kwargs = Base.structdiff(solver_kwargs, NamedTuple{(:x0,)})
-  solve!(solver, reg_nlp, stats_optimized; x = x0, optimized_solver_kwargs...) # It would be interesting to check for allocations here as well but depending on 
+  solve!(solver_object, reg_nlp, stats_optimized; x = x0, optimized_solver_kwargs...) # It would be interesting to check for allocations here as well but depending on 
   # the structure of solver_kwargs, some variables might get boxed, resulting in 
   # false positives, for example if tol = 1e-3; solver_kwargs = (atol = tol),
   # then wrappedallocs would give a > 0 answer...
@@ -41,7 +38,7 @@ function test_solver(
   @test stats_optimized.status == expected_status
   @test obj(reg_nlp, stats_optimized.solution) == stats_optimized.objective
   @test stats_optimized.objective <= obj(reg_nlp, x0)
-  @test all(solver.mν∇fk + solver.∇fk/stats_optimized.solver_specific[:sigma_cauchy] .≤ eps(eltype(solver.mν∇fk)))
+  @test all(solver_object.mν∇fk + solver_object.∇fk/stats_optimized.solver_specific[:sigma_cauchy] .≤ eps(eltype(solver_object.mν∇fk)))
 
   # TODO: test that the optimized entries in stats_optimized and stats_basic are the same.
 
