ea6fa12a90d4ee74c7fee6db2f75510c40eb392b
Maxence Gollier <mgollier45@gmail.com>
2026-02-20 11:08:33 -0500

    R2N & R2DH: add cauchy step stopping criterion

--- test/runtests.jl
+++ test/runtests.jl
@@ -135,6 +135,20 @@ for (mod, mod_name) ∈ (
         @test typeof(out.dual_feas) == eltype(out.solution)
         @test out.status == :first_order
         @test out.step_status == (out.iter > 0 ? :accepted : :unknown)
+
+        # Test with the different stopping criteria
+        out = solver(mod(bpdn), h, options; x0 = x0, atol_decr = 1e-6, rtol_decr = 1e-6, atol_step = 0.0, rtol_step = 0.0)
+        @test typeof(out.solution) == typeof(bpdn.meta.x0)
+        @test length(out.solution) == bpdn.meta.nvar
+        @test typeof(out.dual_feas) == eltype(out.solution)
+        @test out.status == :first_order
+
+        out = solver(mod(bpdn), h, options; x0 = x0, atol_decr = 0.0, rtol_decr = 0.0, atol_step = 1e-6, rtol_step = 1e-6)
+        @test typeof(out.solution) == typeof(bpdn.meta.x0)
+        @test length(out.solution) == bpdn.meta.nvar
+        @test typeof(out.dual_feas) == eltype(out.solution)
+        @test out.status == :first_order
+
       end
     end
   end
