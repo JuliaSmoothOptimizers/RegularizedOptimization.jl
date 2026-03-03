7f4e0cdd66dad936c317be2317755c160a799b5e
Dominique <dominique.orban@gmail.com>
2026-03-03 18:32:47 -0500

    Fix typo in problem constructor

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -10,6 +10,6 @@ for solver ∈ (R2,)
   SUITE[solver_name]["bpdn_l0"] = @benchmarkable $solver($bpdn_l0)
   bpdn_l1, _ = setup_bpdn_l1()
   SUITE[solver_name]["bpdn_l1"] = @benchmarkable $solver($bpdn_l1)
-  bpdn_B0, _ = setup_bpdn_lB()
+  bpdn_B0, _ = setup_bpdn_B0()
   SUITE[solver_name]["bpdn_B0"] = @benchmarkable $solver($bpdn_B0)
 end
