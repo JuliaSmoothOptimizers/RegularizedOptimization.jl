1430b4d48264db3d84a7b887c88743d9f228cc3c
Dominique <dominique.orban@gmail.com>
2026-03-03 15:58:35 -0500

    begin integrating JSOBenchmarks

---
+++ benchmarks/benchmarks.jl
@@ -0,0 +1,15 @@
+using BenchmarkTools
+using RegularizedProblems, RegularizedOptimization
+
+const SUITE = BenchmarkGroup()
+
+for solver ∈ (R2,)
+  solver_name = string(solver)
+  SUITE[solver_name] = BenchmarkGroup()
+  bpdn_l0, _ = setup_bpdn_l0()
+  SUITE[solver_name]["bpdn_l0"] = @benchmarkable $solver($bpdn_l0)
+  bpdn_l1, _ = setup_bpdn_l1()
+  SUITE[solver_name]["bpdn_l1"] = @benchmarkable $solver($bpdn_l1)
+  bpdn_B0, _ = setup_bpdn_lB()
+  SUITE[solver_name]["bpdn_B0"] = @benchmarkable $solver($bpdn_B0)
+end
