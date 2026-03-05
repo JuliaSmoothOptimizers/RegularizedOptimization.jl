92d88cd56362fb6344f31af53e46e10a722db941
MaxenceGollier <mgollier45@gmail.com>
2026-03-05 10:56:59 -0500

    starat SolverBenchmark

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -1,16 +1,31 @@
-using BenchmarkTools
-using RegularizedProblems, RegularizedOptimization
-
-const SUITE = BenchmarkGroup()
+using RegularizedProblems, RegularizedOptimization, SolverBenchmark
 
 bpdn_l0, _ = setup_bpdn_l0()
 bpdn_l1, _ = setup_bpdn_l1()
 bpdn_B0, _ = setup_bpdn_B0()
 
-for solver ∈ (R2,)
-  solver_name = string(solver)
-  SUITE[solver_name] = BenchmarkGroup()
-  SUITE[solver_name]["bpdn_l0"] = @benchmarkable $solver($bpdn_l0)
-  SUITE[solver_name]["bpdn_l1"] = @benchmarkable $solver($bpdn_l1)
-  SUITE[solver_name]["bpdn_B0"] = @benchmarkable $solver($bpdn_B0)
-end
+problem_list = [
+  bpdn_l0,
+  bpdn_l1,
+  bpdn_B0,
+]
+
+solvers = Dict(
+  :R2_precise => 
+    reg_nlp -> R2(
+      reg_nlp,
+      verbose = 0,
+      atol = 1e-6,
+      rtol = 1e-6,
+    ),
+  :R2_imprecise => 
+    reg_nlp -> R2(
+      reg_nlp,
+      verbose = 0,
+      atol = 1e-3,
+      rtol = 1e-3,
+    )
+)
+
+stats = bmark_solvers(solvers, problem_list)
+
