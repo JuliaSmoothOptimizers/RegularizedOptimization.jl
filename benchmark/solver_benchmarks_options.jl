1471b19f15d91573ab2be7419594bcba3919c915
MaxenceGollier <mgollier45@gmail.com>
2026-03-11 18:21:00 -0400

    add options

---
+++ benchmark/solver_benchmarks_options.jl
@@ -0,0 +1,7 @@
+function solver_benchmark_profile_values()
+  [(:elapsed_time, "CPU Time"), (:neval_obj, "# Objective Evals"), (:neval_grad, "# Gradient Evals"), (:niter, "# Iterations")]
+end
+
+function solver_benchmark_table_values()
+  return [(:name, "Name"), (:objective, "f(x)"), (:elapsed_time, "Time"), (:neval_obj, "Obj Evals"), (:neval_grad, "Grad Evals"), (:niter, "Iterations")]
+end
