69d2b885a01acb61387a25e4d5976a4cd68f43ef
MaxenceGollier <mgollier45@gmail.com>
2026-03-11 18:10:24 -0400

    add options

--- benchmark/solver_benchmarks.jl
+++ benchmark/solver_benchmarks.jl
@@ -1,14 +1,6 @@
 using Random
 using RegularizedProblems, RegularizedOptimization, SolverBenchmark
 
-function solver_benchmark_profile_values()
-  [(:elapsed_time, "CPU Time"), (:neval_obj, "# Objective Evals"), (:neval_grad, "# Gradient Evals"), (:niter, "# Iterations")]
-end
-
-function solver_benchmark_table_values()
-  return [(:name, "Name"), (:objective, "f(x)"), (:elapsed_time, "Time"), (:neval_obj, "Obj Evals"), (:neval_grad, "Grad Evals"), (:niter, "Iterations")]
-end
-
 Random.seed!(0)
 
 bpdn_l0, _ = setup_bpdn_l0()
