e589d65f2788b77b63350c3cfc492217a0c2cb4c
MaxenceGollier <mgollier45@gmail.com>
2026-03-13 14:10:31 -0400

    add options directly into the benchmark script

--- benchmark/solver_benchmarks_options.jl
+++
@@ -1,7 +0,0 @@
-function solver_benchmark_profile_values()
-  [(:elapsed_time, "CPU Time"), (:neval_obj, "# Objective Evals"), (:neval_grad, "# Gradient Evals"), (:iter, "# Iterations")]
-end
-
-function solver_benchmark_table_values()
-  return [(:name, "Name"), (:objective, "f(x)"), (:elapsed_time, "Time"), (:neval_obj, "Obj Evals"), (:neval_grad, "Grad Evals"), (:iter, "Iterations")]
-end
