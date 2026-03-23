ae0a919aad76e1708356d142289b715402893166
MaxenceGollier <mgollier45@gmail.com>
2026-03-23 11:47:44 -0400

    add all solvers to the benchmarks

--- benchmark/solver_benchmarks.jl
+++ benchmark/solver_benchmarks.jl
@@ -24,6 +24,7 @@ problem_list = [
 ]
 
 solvers = Dict(
+  # R2
   :R2_precise => 
     reg_nlp -> R2(
       reg_nlp,
@@ -37,7 +38,138 @@ solvers = Dict(
       verbose = 1,
       atol = 1e-3,
       rtol = 1e-3,
-    )
+    ),
+  :R2_default => 
+    reg_nlp -> R2(
+      reg_nlp,
+      verbose = 1,
+    ),
+  
+  # R2N with BFGS
+  :R2N_bfgs_precise => 
+    reg_nlp -> R2N(
+      RegularizedNLPModel(LBFGSModel(reg_nlp.model), reg_nlp.h, reg_nlp.selected),
+      verbose = 1,
+      atol = 1e-6,
+      rtol = 1e-6,
+    ),
+  :R2N_bfgs_imprecise =>
+    reg_nlp -> R2N(
+      RegularizedNLPModel(LBFGSModel(reg_nlp.model), reg_nlp.h, reg_nlp.selected),
+      verbose = 1,
+      atol = 1e-3,
+      rtol = 1e-3,
+    ),
+  :R2N_bfgs_default =>
+    reg_nlp -> R2N(
+      RegularizedNLPModel(LBFGSModel(reg_nlp.model), reg_nlp.h, reg_nlp.selected),
+      verbose = 1,
+    ),
+  
+  # R2N with SR1
+  :R2N_sr1_precise => 
+    reg_nlp -> R2N(
+      RegularizedNLPModel(LSR1Model(reg_nlp.model), reg_nlp.h, reg_nlp.selected),
+      verbose = 1,
+      atol = 1e-6,
+      rtol = 1e-6,
+    ),
+  :R2N_sr1_imprecise =>
+    reg_nlp -> R2N(
+      RegularizedNLPModel(LSR1Model(reg_nlp.model), reg_nlp.h, reg_nlp.selected),
+      verbose = 1,
+      atol = 1e-3,
+      rtol = 1e-3,
+    ),
+  :R2N_sr1_default =>
+    reg_nlp -> R2N(
+      RegularizedNLPModel(LSR1Model(reg_nlp.model), reg_nlp.h, reg_nlp.selected),
+      verbose = 1,
+    ),
+
+  # TR with BFGS
+  :TR_bfgs_precise => 
+    reg_nlp -> TR(
+      RegularizedNLPModel(LBFGSModel(reg_nlp.model), reg_nlp.h, reg_nlp.selected),
+      verbose = 1,
+      atol = 1e-6,
+      rtol = 1e-6,
+    ),
+  :TR_bfgs_imprecise =>
+    reg_nlp -> TR(
+      RegularizedNLPModel(LBFGSModel(reg_nlp.model), reg_nlp.h, reg_nlp.selected),
+      verbose = 1,
+      atol = 1e-3,
+      rtol = 1e-3,
+    ),
+  :TR_bfgs_default =>
+    reg_nlp -> TR(
+      RegularizedNLPModel(LBFGSModel(reg_nlp.model), reg_nlp.h, reg_nlp.selected),
+      verbose = 1,
+    ),
+  
+  # TR with SR1
+  :TR_sr1_precise => 
+    reg_nlp -> TR(
+      RegularizedNLPModel(LSR1Model(reg_nlp.model), reg_nlp.h, reg_nlp.selected),
+      verbose = 1,
+      atol = 1e-6,
+      rtol = 1e-6,
+    ),
+  :TR_sr1_imprecise =>
+    reg_nlp -> TR(
+      RegularizedNLPModel(LSR1Model(reg_nlp.model), reg_nlp.h, reg_nlp.selected),
+      verbose = 1,
+      atol = 1e-3,
+      rtol = 1e-3,
+    ),
+  :TR_sr1_default =>
+    reg_nlp -> TR(
+      RegularizedNLPModel(LSR1Model(reg_nlp.model), reg_nlp.h, reg_nlp.selected),
+      verbose = 1,
+    ),
+
+  # R2DH
+  :R2DH_precise => 
+    reg_nlp -> R2DH(
+      reg_nlp,
+      verbose = 1,
+      atol = 1e-6,
+      rtol = 1e-6,
+    ),
+  :R2DH_imprecise =>
+    reg_nlp -> R2DH(
+      reg_nlp,
+      verbose = 1,
+      atol = 1e-3,
+      rtol = 1e-3,
+    ),
+  :R2DH_default =>
+    reg_nlp -> R2DH(
+      reg_nlp,
+      verbose = 1,
+    ),
+  
+  # TRDH
+  :TRDH_precise => 
+    reg_nlp -> TRDH(
+      reg_nlp,
+      verbose = 1,
+      atol = 1e-6,
+      rtol = 1e-6,
+    ),
+  :TRDH_imprecise =>
+    reg_nlp -> TRDH(
+      reg_nlp,
+      verbose = 1,
+      atol = 1e-3,
+      rtol = 1e-3,
+    ),
+  :TRDH_default =>
+    reg_nlp -> TRDH(
+      reg_nlp,
+      verbose = 1,
+    ),
 )
 
 stats = bmark_solvers(solvers, problem_list)
