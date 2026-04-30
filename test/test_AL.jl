d4907402f1d11d9d04a3bad8559a25b840619be2
dpo <dpo@users.noreply.github.com>
2026-04-30 21:47:22 +0000

    :robot: Format .jl files

--- test/test_AL.jl
+++ test/test_AL.jl
@@ -5,7 +5,8 @@ problem_list = [:hs8]
   for problem in problem_list
     nlp = eval(problem)(backend = :optimized)
     for h in (NormL1(1.0),)
-      stats = AL(nlp, h, subsolver = R2Solver, atol = 1e-3, verbose = 1, sub_kwargs = (max_iter = 1000,))
+      stats =
+        AL(nlp, h, subsolver = R2Solver, atol = 1e-3, verbose = 1, sub_kwargs = (max_iter = 1000,))
       @test stats.status == :first_order
       @test stats.primal_feas <= 1e-2
       @test stats.dual_feas <= 1e-2
